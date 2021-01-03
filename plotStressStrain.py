a964d7e1fa485acd27f42775162af4c8e1cabd68
anhvt2 <anh.vt2@gmail.com>
2021-01-03 00:53:04 -0500

    update plotStressStrain.py

--- plotStressStrain.py
+++ plotStressStrain.py
@@ -1,17 +1,34 @@
 
 # postResults single_phase_equiaxed_tension.spectralOut --cr f,p
 # filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log
+# python3 plotStressStrain.py --file "stress_strain.log"
+
 import numpy as np
-import matplotlib as mpl
 import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os, sys, datetime
+import argparse
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument("-file", "--file", default='stress_strain.log', type=str)
+parser.add_argument("-optSaveFig", "--optSaveFig", type=bool, default=False)
+args = parser.parse_args()
+file = args.file
 
 mpl.rcParams['xtick.labelsize'] = 24
 mpl.rcParams['ytick.labelsize'] = 24
 
-d = np.loadtxt('stress_strain.log', skiprows=7)
+d = np.loadtxt(file, skiprows=7)
+vareps = d[:,1] # strain
+sigma  = d[:,2] # stress
 
-plt.plot(d[:,1] - 1, d[:,2], 'bo-', markersize=2)
-plt.xlabel(r'$\varepsilon$', fontsize=30)
-plt.ylabel(r'$\sigma$', fontsize=30)
+fig = plt.figure()
+ax = fig.add_subplot(111)
+ax.plot((vareps - 1), sigma / 1e6, 'bo-', markersize=2)
+plt.xlabel(r'$\varepsilon$ ', fontsize=30)
+plt.ylabel(r'$\sigma$ [MPa]', fontsize=30)
+
+ax.xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.4f'))
+
 plt.show()
 

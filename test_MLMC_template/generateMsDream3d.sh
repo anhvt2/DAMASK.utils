1e5dfbcdfe6ed9e924ffaaeaf0a5edfb113561c5
Anh Tran <anh.vt2@gmail.com>
2021-04-16 18:10:29 -0400

    add a wrapper file (wrapper_DREAM3D-DAMASK.py) around DREAM3D and DAMASK

--- test_MLMC_template/generateMsDream3d.sh
+++ test_MLMC_template/generateMsDream3d.sh
@@ -12,10 +12,11 @@ dimCell=$(cat dimCell.dat)
 
 ## declare paths
 
-execPath="/ascldap/users/anhtran/data/DREAM.3D/DREAM3D-6.5.138-Linux-x86_64/bin"
+# execPath="/ascldap/users/anhtran/data/DREAM.3D/DREAM3D-6.5.138-Linux-x86_64/bin"
+execPath="/home/anhvt89/Documents/DREAM.3D/DREAM3D-6.5.128-Linux-x86_64/bin"
 outputPath=$(pwd)
 
-inputFile="test-DownSamplingSVEs-solo.json"
+inputFile="test-DownSamplingSVEs-ideapad320-NonExact-base320.json"
 currentPath="${inputPath}"
 
 
@@ -36,13 +37,20 @@ sed -i "54s|.*|                    \"Standard Deviation\": ${sigma}|" ${inputFil
 sed -i "140s|.*|            \"x\": ${dimCell},|" ${inputFile}
 sed -i "141s|.*|            \"y\": ${dimCell},|" ${inputFile}
 sed -i "142s|.*|            \"z\": ${dimCell}|" ${inputFile}
-sed -i "388s|.*|        \"OutputPath\": \"${outputPath}/72x72x72\"|" ${inputFile}
-sed -i "445s|.*|        \"OutputPath\": \"${outputPath}/60x60x60\"|" ${inputFile}
-sed -i "502s|.*|        \"OutputPath\": \"${outputPath}/48x48x48\"|" ${inputFile}
-sed -i "559s|.*|        \"OutputPath\": \"${outputPath}/36x36x36\"|" ${inputFile}
-sed -i "616s|.*|        \"OutputPath\": \"${outputPath}/24x24x24\"|" ${inputFile}
-sed -i "673s|.*|        \"OutputPath\": \"${outputPath}/18x18x18\"|" ${inputFile}
-sed -i "730s|.*|        \"OutputPath\": \"${outputPath}/12x12x12\"|" ${inputFile}
+
+
+## NOTE: This action could be automated by (1) searching for the line number with this pattern and (2) replacing the found line number in a FOR loop
+# sed -i "388s|.*|        \"OutputPath\": \"${outputPath}/72x72x72\"|" ${inputFile}
+# sed -i "445s|.*|        \"OutputPath\": \"${outputPath}/60x60x60\"|" ${inputFile}
+# sed -i "502s|.*|        \"OutputPath\": \"${outputPath}/48x48x48\"|" ${inputFile}
+# sed -i "559s|.*|        \"OutputPath\": \"${outputPath}/36x36x36\"|" ${inputFile}
+# sed -i "616s|.*|        \"OutputPath\": \"${outputPath}/24x24x24\"|" ${inputFile}
+# sed -i "673s|.*|        \"OutputPath\": \"${outputPath}/18x18x18\"|" ${inputFile}
+# sed -i "730s|.*|        \"OutputPath\": \"${outputPath}/12x12x12\"|" ${inputFile}
+
+sed -i "651s|.*|        \"OutputPath\": \"${outputPath}/16x16x16\"|" ${inputFile}
+sed -i "708s|.*|        \"OutputPath\": \"${outputPath}/8x8x8\"|" ${inputFile}
+
 
 ${execPath}/PipelineRunner -p $(pwd)/${inputFile}
 
@@ -53,7 +61,8 @@ echo
 echo
 
 export geom_check=/ascldap/users/anhtran/data/DAMASK/DAMASK-2.0.2/processing/pre/geom_check.sh
-for dimCell in 72 60 48 36 24 18 12; do
+# for dimCell in 72 60 48 36 24 18 12; do
+for dimCell in 16 8; do
 	cd ${dimCell}x${dimCell}x${dimCell}
 
 	cat ../material.config.preamble  | cat - material.config | sponge material.config
---
+++ test_MLMC_template/wrapper_DREAM3D-DAMASK.py
@@ -0,0 +1,95 @@
+
+""" 
+PURPOSES:
+
+This script "wrapper_DREAM3D-DAMASK.py":
+
+1. Generates a multi-level SVE approximation of a microstructure realization by DREAM.3D
+	* Each folder corresponds to a unique SVE mesh size
+	-- This is done by calling "generateMsDream3d.sh"
+2. Pre-process the microstructure geometry
+	-- This is done by "geom_check *.geom"
+3. Submit a DAMASK job on Solo with one or more particular level(s) of interests
+	* This includes the post-processing step
+	* Dump out the quantities of interests (e.g. "yield.out")
+	-- This is done by calling "ssubmit sbatch.damask.solo"
+4. Collect the QoI and return to Julia "MultilevelEstimators.jl"
+
+NOTE: 
+
+1. "generateMsDream3d.sh" and subsequently the .json file used by DREAM.3D can be automatically generated, but currently we assume they are fixed and hard-coded.
+
+For example, the number of levels can (but not yet) be adaptively changed and controlled as well as the size of SVEs
+
+2. DREAM.3D automatically creates folder if it doesn't exist.
+"""
+
+import numpy as np
+import os, glob
+import argparse
+import time
+import datetime
+
+# adopt from Sandwich.py and Example.jl from Pieterjan Robbe (KU Leuven)
+
+## python3 Sandwich.py --elemx $(hex) --elemy $(hey) --young $(Ed1) $(Ed2)
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-meshSize", "--meshSize", type=int)
+# parser.add_argument("-baseSize", "--baseSize", default=320, type=int) # unnecessary -- unless generateMsDream3d.sh is adaptive then this parameter is fixed for now
+# NOTE: note that "generateMsDream3d.sh" is hard-coded with a specific number of levels and a specific set of lines to change
+
+args = parser.parse_args()
+meshSize = int(args.meshSize) # make sure meshSize is integer
+
+# generate all the meshSize but only run in the selected meshSize
+# NOTE: meshSize must be divisible by the base
+# for example: base 320 generate 320x320x320 SVE initially
+# 			then the meshSize could be 64, 32, and so on
+
+# for testing purposes: probably best to test with small size 8x8x8 (2 procs) or 16x16x16 (4 procs)
+
+
+## generate ALL microstructure approximations
+currentDirectory = os.getcwd() # get currentDirectory for reference
+os.system('sh generateMsDream3d.sh')
+os.chdir(currentDirectory + '/%dx%dx%d' % (meshSize, meshSize, meshSize)) # go into subfolder "${meshSize}x${meshSize}x${meshSize}"
+
+# write down numProcessors to be picked up later by sbatch.damask.solo
+numProcessors = floor(meshSize / 4.)
+if numProcessors > 36:
+	numProcessors = 36 # threshold on Solo node
+
+f = open('numProcessors.dat', 'w') # can be 'r', 'w', 'a', 'r+'
+f.write('%d' % numProcessors)
+f.close()
+
+os.system('ssubmit sbatch.damask.solo')
+# os.chdir(currentDirectory + '/%dx%dx%d' % (meshSize, meshSize, meshSize) + '/postProc')
+
+startTime = datetime.datetime.now()
+
+## get thresholdSlurmTime adaptively from sbatch.damask.solo (or just hard-code it, e.g. 48 * 24 * 3600)
+# return thresholdSlurmTime in seconds, read from sbatch.damask.solo
+slurmFile = open(currentDirectory + '/%dx%dx%d' % (meshSize, meshSize, meshSize) + '/sbatch.damask.solo')
+slurmSubmitText = slurmFile.readlines()
+slurmFile.close()
+thresholdSlurmTime = slurmSubmitText[3].split('=')[1].split("#")[0].replace(" ", "") # e.g. thresholdSlurmTime = "48:00:00"
+thresholdSlurmTime = thresholdSlurmTime.split(":")[0]
+thresholdSlurmTime = int(thresholdSlurmTime)
+thresholdSlurmTime *= (24*3600) # convert to seconds
+
+while not os.path.exists(currentDirectory + '/%dx%dx%d' % (meshSize, meshSize, meshSize) + '/postProc/yield.out'):
+	time.sleep(10)
+	currentTime = datetime.datetime.now()
+	if (currentTime - startTime).total_seconds() > thresholdSlurmTime:
+		break
+else:
+	yieldData = np.loadtxt(currentDirectory + '/%dx%dx%d' % (meshSize, meshSize, meshSize) + '/postProc/yield.out')
+	yieldStrain = float(yieldData[0])
+	yieldStress = float(yieldData[1]) / 1e9 # in GPa
+
+
+
+
+

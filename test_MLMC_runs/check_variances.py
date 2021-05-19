4b7a65df50360436ffc97699bd8c5a7c00d90035
Anh Tran <anh.vt2@gmail.com>
2021-05-19 13:23:15 -0400

    remove safety condition for check_variances.py

--- test_MLMC_runs/check_variances.py
+++ test_MLMC_runs/check_variances.py
@@ -14,16 +14,12 @@ def sample_cpfem(level):
                 Q.append(yield_stress) # append the value of the yield stress to the array "Q"
             except:
                 Q.append(None)
-    print(Q)
-    if len(Q) == 2: # safety check if 
-        if level == 0:
-            return Q[0], Q[0] # return twice Qf
-        else:
-            return Q[0] - Q[1], Q[0] # return multilevel difference and Qf
+    if level == 0:
+        return Q[0], Q[0] # return twice Qf
     else:
-        return None, None
+        return Q[0] - Q[1], Q[0] # return multilevel difference and Qf
 
-def check_variances(max_level=3, budget=3600*24*2):
+def check_variances(max_level=3, budget=3600*24):
     buget_per_level = budget/(max_level + 1)
 
     for level in range(max_level):

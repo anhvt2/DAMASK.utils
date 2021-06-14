81aab7eab0e74aa278e09994cc57c87325b8ddf1
Anh Tran <anh.vt2@gmail.com>
2021-06-14 13:58:00 -0400

    add parallel thread check_variances.jl in 11Jun21

--- MultilevelEstimators.jl-extras/11Jun21/check_variances.jl
+++ MultilevelEstimators.jl-extras/11Jun21/check_variances.jl
@@ -10,7 +10,7 @@ using PrettyTables
 using ProgressMeter
 
 # Command to run multi-index version of DREAM3D-DAMASK
-get_cmd(index::Index) = Cmd(["python3", "wrapperMLMC_DREAM3D-DAMASK.py", "--index", string(index)])
+get_cmd(index::Index) = Cmd(["python3", "wrapper_DREAM3D-DAMASK.py", "--index", string(index)])
 
 # Read DREAM3D-DAMASK wrapper output and return value of estimated yield
 # stress for the given level or index
@@ -45,15 +45,16 @@ highlight_cols(k) = Highlighter((v, i, j) -> any(j .== k), get_crayon())
 
 # Print summary statistics
 function print_table(data)
+	indices = sort(collect(keys(data)))
     ell_name = length(first(keys(data))) > 1 ? "Index" : "Level"
     header = [ell_name, "|Eℓ|", "|ΔEℓ|", "Vℓ", "ΔVℓ", "Nℓ", "Wℓ"]
-    E = [abs(mean(val[2])) for (key, val) in data]
-    dE = [abs(mean(val[1])) for (key, val) in data]
-    V = [var(val[2]) for (key, val) in data]
-    dV = [var(val[1]) for (key, val) in data]
-    N = [length(val[1]) for (key, val) in data]
-    W = [length(val[1])/val[3] for (key, val) in data]
-    entries = hcat(sort(collect(keys(data))), E, dE, V, dV, N, W)
+	E = [abs(mean(data[index][2])) for index in indices]
+	dE = [abs(mean(data[index][1])) for index in indices]
+	V = [var(data[index][2]) for index in indices]
+	dV = [var(data[index][1]) for index in indices]
+	N = [length(data[index][1]) for index in indices]
+	W = [data[index][3]/length(data[index][1]) for index in indices]
+    entries = hcat(indices, E, dE, V, dV, N, W)
     e_fmt = ft_printf("%5.3e", [2, 3, 4, 5, 7])
     pretty_table(entries,
                  header,
@@ -61,7 +62,7 @@ function print_table(data)
                  header_alignment=:c, 
                  formatters = e_fmt,
                  highlighters=highlight_cols(1),
-                 equal_columns_width=true)
+				 equal_columns_width=true)
 end
 
 """
@@ -79,19 +80,21 @@ function check_variances(; index_set=ML(), max_level=3, budget=20)
 
     # Loop over all levels/indices
     for index in indices
-        samples_Qf = [] # vector to store samples of Qf
-        samples_dQ = [] # vector to store samples of dQ
+		samples = [Vector{NTuple{2, Float64}}(undef, 0) for _ in 1:Threads.nthreads()]
         timer = 0
         p = Progress(budget_per_level, dt=1, barglyphs=BarGlyphs("[=> ]"), color=:none)
         while timer < budget_per_level # run timer until we run out of buget
-            timer += @elapsed dQ, Qf = sample(index) # take a new sample
-            if isfinite(dQ) && isfinite(Qf) # check if the sample is valid
-                push!(samples_dQ, dQ) # update dQ
-                push!(samples_Qf, Qf) # update Qf
-            end
+            timer += @elapsed begin
+				Threads.@threads for _ in 1:Threads.nthreads()
+					dQ, Qf = sample(index) # take a new sample
+					if isfinite(dQ) && isfinite(Qf) # check if the sample is valid
+						push!(samples[Threads.threadid()], (dQ, Qf))
+					end
+				end
+			end
             update!(p, min(budget_per_level, floor(Int, timer))) # update progress bar
         end
-        data[index] = (samples_dQ, samples_Qf, timer) # add new key to the dictionary
+		data[index] = (first.(vcat(samples...)), last.(vcat(samples...)), timer) # add new key to the dictionary
         print_table(data) # print the results
     end
 end

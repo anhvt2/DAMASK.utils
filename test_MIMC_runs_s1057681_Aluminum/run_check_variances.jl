99ae4f071d43b1cea590bf9922589f0cee6f240d
Anh Tran <anh.vt2@gmail.com>
2021-07-28 17:11:18 -0400

    update run_check_variances.jl

--- test_MIMC_runs_s1057681_Aluminum/run_check_variances.jl
+++ test_MIMC_runs_s1057681_Aluminum/run_check_variances.jl
@@ -14,7 +14,7 @@ include("check_variances.jl") # load check_variances
 # index_set = ML() # OPTION 1: multilevel
 
 # index_set = TD(3) # OPTION 2: multi-index, total degree
-index_set = TD(1, 2/3) # weighted index set
+index_set = TD(1, (3-1)/(5-1)) # weighted index set
 # index_set = TD(1, 2/4) # weighted index set
 
 # index_set = FT(3) # OPTION 3: multi-index, full tensor
@@ -24,7 +24,7 @@ index_set = TD(1, 2/3) # weighted index set
 # ========================
 # >>> Select maximum level parameter:
 
-max_level = 3 # See note below
+max_level = 2 # See note below
 
 # @@@ NOTE @@@
 # !!! In a multilevel setting, `max_level=3` means 4 levels in total (i.e.,
@@ -57,7 +57,7 @@ max_level = 3 # See note below
 # =============================
 # >>> Specify total run time (in seconds):
 
-budget = 3*24*3600 # this means 20 seconds, should be much larger in your example
+budget = 4*24*3600 # this means 20 seconds, should be much larger in your example
 
 # @@@ NOTE @@@
 # !!! You can use for example the following specifications:

237cefb2c88a38ff62deb02911d77978d5d7ed3f
Anh Tran <anh.vt2@gmail.com>
2021-10-13 11:47:11 -0600

    debug Phase_Nonlocal_Nickel.config and its preamble: burgers wrong specification

--- test_MIMC_runs_s1057681_Nickel/8x8x8_Nonlocal/run_damask.sh
+++ test_MIMC_runs_s1057681_Nickel/8x8x8_Nonlocal/run_damask.sh
@@ -22,10 +22,11 @@ source $DAMASK_ROOT/env/DAMASK.sh
 # source /ascldap/users/anhtran/data/DAMASK/damask-3.0.0-alpha/env/DAMASK.sh
 
 echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>"
-echo "Start running simulation at"
+echo "Start running simulation at:"
 echo $(date +%y-%m-%d-%H-%M-%S)
-echo "from directory"
+echo "from:"
 echo $(pwd)
+echo
 
 ### ---------------------------------- pre-set on Solo
 
@@ -75,7 +76,7 @@ if [ -d "postProc" ]; then
 		python3 ../computeYieldStress.py
 	elif [[ $1 == "YoungModulus" ]]; then
 		python3 ../computeYoungModulus.py
-	else 
+	else
 		echo "run_damask_2.0.3.sh: \$1 argument is not detected in run_damask_2.0.3.sh"
 	fi
 
@@ -92,3 +93,6 @@ fi
 echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<"
 echo "Stop running simulation at"
 echo $(date +%y-%m-%d-%H-%M-%S)
+echo "from :"
+echo $(pwd)
+echo

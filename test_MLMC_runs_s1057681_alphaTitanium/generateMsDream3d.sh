6ce6a84536e8ff0ef898a4a608d0436acca6de05
Anh Tran <anh.vt2@gmail.com>
2021-10-13 18:09:48 -0400

    debug: update username in DREAM3D exec path

--- test_MLMC_runs_s1057681_alphaTitanium/generateMsDream3d.sh
+++ test_MLMC_runs_s1057681_alphaTitanium/generateMsDream3d.sh
@@ -21,7 +21,7 @@ elif [[ ${hostName} == *"ideapad"* ]]; then
 elif [[ ${hostName} == *"s1057681"* ]]; then
 	execPath="/home/anhtran/Documents/DREAM.3D/DREAM3D-6.5.141-Linux-x86_64/bin" # s1057681
 elif [[ ${hostName} == *"strix"* ]]; then
-	execPath="/home/anhtran/Documents/DREAM.3D/DREAM3D-6.5.141-Linux-x86_64/bin" # Asus ROG strix
+	execPath="/home/anhvt89/Documents/DREAM.3D/DREAM3D-6.5.141-Linux-x86_64/bin" # Asus ROG strix
 else
 	execPath="/home/anhvt89/Documents/DREAM.3D/DREAM3D-6.5.128-Linux-x86_64/bin" # else
 fi

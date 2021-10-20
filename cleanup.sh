c6840c76bddba834938c040c8ddd9ca5af1fee25
Anh Tran <anh.vt2@gmail.com>
2021-10-20 14:29:07 -0400

    add cleanup.sh for removing folders

---
+++ cleanup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+rm -rfv 16x16x16_Phenopowerlaw/
+rm -rfv 16x16x16_Nonlocal/
+rm -rfv 20x20x20_Phenopowerlaw/
+rm -rfv 20x20x20_Nonlocal/
+rm -rfv 32x32x32_Phenopowerlaw/
+rm -rfv 32x32x32_Nonlocal/
+rm -rfv 64x64x64_Phenopowerlaw/
+rm -rfv 64x64x64_Nonlocal/
+rm -rfv 10x10x10_Nonlocal/
+rm -rfv 8x8x8_Nonlocal/
+rm -rfv 10x10x10_Phenopowerlaw/
+rm -rfv 8x8x8_Phenopowerlaw/
+

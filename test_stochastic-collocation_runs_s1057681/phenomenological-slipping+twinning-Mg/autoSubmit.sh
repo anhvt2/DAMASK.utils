11bd3f75bcbf40384505cf4295b306033c3cfe0c
Anh Tran <anh.vt2@gmail.com>
2022-03-09 17:11:51 -0500

    add post-processing results for phenomenological-slipping+twinning-Mg

--- test_stochastic-collocation_runs_s1057681/phenomenological-slipping+twinning-Mg/autoSubmit.sh
+++ test_stochastic-collocation_runs_s1057681/phenomenological-slipping+twinning-Mg/autoSubmit.sh
@@ -1,22 +1,17 @@
 #!/bin/bash
 
-ii=0
-maxSubmit=99999
-
-for folderName in $(ls -1dv sg_input_*/); do
+# for folderName in $(ls -1dv sg_input_*/); do
+for folderName in $(cat submit.log); do
 	cd $folderName
 
-	if [ -d "postProc" ]; then
-		echo "postProc/ is available in $(basename $(pwd))"
-	else
-		if [ "$ii" -lt "$maxSubmit" ]; then
-			sdel
-			ssubmit
-			((ii++))
-			echo "Re-submit job in $(basename $(pwd))"
-			echo "$(basename $(pwd))" >> ../submit.log
-		fi
-	fi
+#	if [ -d "postProc" ]; then
+#		echo "postProc/ is available in $(basename $(pwd))"
+#	else
+		sdel
+		ssubmit
+		echo "Re-submit job in $(basename $(pwd))"
+#		echo "$(basename $(pwd))" >> ../submit.log
+#	fi
 
 	echo "done $folderName"
 	cd ..

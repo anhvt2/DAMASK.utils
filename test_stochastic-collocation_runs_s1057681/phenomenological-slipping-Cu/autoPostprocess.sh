f1237d06121fbdaf6c3b746d913c2640c24065f1
Anh Tran <anh.vt2@gmail.com>
2022-03-16 14:50:56 -0400

    update Cu/ post-processing results

--- test_stochastic-collocation_runs_s1057681/phenomenological-slipping-Cu/autoPostprocess.sh
+++ test_stochastic-collocation_runs_s1057681/phenomenological-slipping-Cu/autoPostprocess.sh
@@ -1,17 +1,14 @@
 #!/bin/bash
 
+rm -fv output.dat
 for folderName in $(ls -1dv sg_input_*/); do
-	cd $folderName
+	cd $folderName/
 
-
-	if [ -d "postProc" ]; then
-		echo "postProc/ is available in $(basename $(pwd))"
-		cd postProc
-		python3 ../../computeYieldStress.py
-		cd ..
-	else
-		echo "postProc/ is NOT available in $(basename $(pwd))"
-	fi
+	cat output.dat >> ../output.dat
+	# cd postProc/
+	# rm -fv output.dat postProc/output.dat
+	# python3 ../../computeYieldStress.py
+	# cd ..
 
 	echo "done $folderName"
 	cd ..

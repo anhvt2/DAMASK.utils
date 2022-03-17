aa61243be3c56904713ace1888fd0f1d85c4551f
Anh Tran <anh.vt2@gmail.com>
2022-03-16 19:31:16 -0600

    update Cu/ results from s1057681
    * after an incorrect merge conflict from Solo

---
+++ test_stochastic-collocation_runs_s1057681/phenomenological-slipping-Cu/test.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+cd sg_input_5; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_9; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_11; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_23; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_25; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_30; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_33; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_55; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_87; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_90; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_103; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_118; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_179; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_210; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_215; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_216; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_230; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_232; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_252; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_276; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_285; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_297; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_330; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_331; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+cd sg_input_338; postResults single_phase_equiaxed_tension.spectralOut --cr f,p; cd postProc/; filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log; cd ..; cd ..;
+

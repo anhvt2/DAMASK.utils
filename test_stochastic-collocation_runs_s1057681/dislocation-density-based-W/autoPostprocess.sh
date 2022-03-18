6bfeb1ac939da699e83f596fb227310aa79e8941
Anh Tran <anh.vt2@gmail.com>
2022-03-18 00:12:20 -0400

    update W/ post-processing scripts

--- test_stochastic-collocation_runs_s1057681/dislocation-density-based-W/autoPostprocess.sh
+++ test_stochastic-collocation_runs_s1057681/dislocation-density-based-W/autoPostprocess.sh
@@ -4,10 +4,10 @@ rm -fv output.dat
 for folderName in $(ls -1dv sg_input_*/); do
 	cd $folderName/
 
+#	cd postProc/
+#	python3 ../../computeYieldStress.py
+#	cd ..
 	cat output.dat >> ../output.dat
-	cd postProc/
-	# python3 ../../computeYieldStress.py
-	cd ..
 
 	echo "done $folderName"
 	cd ..

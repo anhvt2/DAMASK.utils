df1a712f8a4e0b8ed2afc4592153a72bc47957f4
Anh Tran <anh.vt2@gmail.com>
2022-03-17 21:37:02 -0400

    update Mg/ post-processing results

--- test_stochastic-collocation_runs_s1057681/phenomenological-slipping+twinning-Mg/autoPostprocess.sh
+++ test_stochastic-collocation_runs_s1057681/phenomenological-slipping+twinning-Mg/autoPostprocess.sh
@@ -4,9 +4,9 @@ rm -fv output.dat
 for folderName in $(ls -1dv sg_input_*/); do
 	cd $folderName/
 
-#	cd postProc/
-#	python3 ../../computeYieldStress.py
-#	cd ..
+	cd postProc/
+	python3 ../../computeYieldStress.py
+	cd ..
 	cat output.dat >> ../output.dat
 
 	echo "done $folderName"

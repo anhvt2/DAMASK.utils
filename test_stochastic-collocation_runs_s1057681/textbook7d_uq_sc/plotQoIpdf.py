6d3cc7c387d79129f29ce5d160cdcd5ef7e47af5
Anh Tran <anh.vt2@gmail.com>
2022-03-21 21:57:01 -0400

    add textbook7d/{strain,stress}Yield.dat for W/

--- test_stochastic-collocation_runs_s1057681/textbook7d_uq_sc/plotQoIpdf.py
+++ test_stochastic-collocation_runs_s1057681/textbook7d_uq_sc/plotQoIpdf.py
@@ -1,5 +1,5 @@
 
-# ../dakota -i textbook5d_uq_sc_pyImport.in > dakota.log
+# ../dakota -i textbook7d_uq_sc_pyImport.in > dakota.log
 # grep -inr ' f1' dakota.log  > tmp.txt
 # sed -i  's/ f1//g' tmp.txt
 

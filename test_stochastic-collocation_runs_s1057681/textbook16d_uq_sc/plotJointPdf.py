a7d18a0696201a2a179f788148c0e5914c9665a4
Anh Tran <anh.vt2@gmail.com>
2022-03-22 11:04:05 -0400

    add plotJointPdf.py and figures

---
+++ test_stochastic-collocation_runs_s1057681/textbook16d_uq_sc/plotJointPdf.py
@@ -0,0 +1,32 @@
+
+import numpy as np
+import seaborn as sns
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import pandas as pd
+
+mpl.rcParams['xtick.labelsize'] = 18
+mpl.rcParams['ytick.labelsize'] = 18
+
+o = np.loadtxt('output.dat', delimiter=',')
+o[:,1] /= 1e6
+df = pd.DataFrame(o, columns = [r"$\varepsilon_Y$",r"$\sigma_Y$"])
+
+g = sns.jointplot(data=df, x=r"$\varepsilon_Y$", y=r"$\sigma_Y$", kind="kde", xlim=(0.0025, 0.008), ylim=(50,150)) #, marker='o')
+# g.plot_joint(sns.kdeplot, color="r", zorder=0, levels=6)
+g.plot_marginals(sns.rugplot, color="r", height=0.0, clip_on=False)
+
+# g = sns.pairplot(df)
+
+
+
+# g.set_xlabel(r"$\varepsilon_Y$", fontsize=24)
+# g.set_ylabel(r"$\sigma_Y$", fontsize=24)
+g.set_axis_labels(xlabel=r"$\varepsilon_Y$", ylabel=r"$\sigma_Y$", fontsize=24)
+# plt.xticks([0.002, 0.003, 0.004])
+import matplotlib.ticker as ticker
+g.ax_joint.xaxis.set_major_locator(ticker.MultipleLocator(0.0015))
+g.ax_joint.yaxis.set_major_locator(ticker.MultipleLocator(10))
+# g.set_title('Correlation plot: W', fontsize=24)
+plt.show()
+

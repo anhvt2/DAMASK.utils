817036cf5f4f3fab469a34a73a894622fc9439fb
Anh Tran <anh.vt2@gmail.com>
2022-03-25 12:14:13 -0600

    update results from s1057681

--- test_stochastic-collocation_runs_s1057681/dislocation-density-based-W/autoSubmit.sh
+++ test_stochastic-collocation_runs_s1057681/dislocation-density-based-W/autoSubmit.sh
@@ -1,17 +1,17 @@
 #!/bin/bash
 
-# for folderName in $(ls -1dv sg_input_*/); do
-for folderName in $(cat submit.log); do
+for folderName in $(ls -1dv sg_input_*/); do
+# for folderName in $(cat submit.log); do
 	cd $folderName
 
-#	if [ -d "postProc" ]; then
-#		echo "postProc/ is available in $(basename $(pwd))"
-#	else
+	if [ -d "postProc" ]; then
+		echo "postProc/ is available in $(basename $(pwd))"
+	else
 		sdel
 		ssubmit
-#		echo "Re-submit job in $(basename $(pwd))"
-#		echo "$(basename $(pwd))" >> ../submit.log
-#	fi
+		echo "Re-submit job in $(basename $(pwd))"
+		echo "$(basename $(pwd))" >> ../submit.log
+	fi
 
 	echo "done $folderName"
 	cd ..
---
+++ test_stochastic-collocation_runs_s1057681/dislocation-density-based-W/test1.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cd sg_input_6; bash ../post_process.sh; cd ..
+cd sg_input_7; bash ../post_process.sh; cd ..
+cd sg_input_8; bash ../post_process.sh; cd ..
+cd sg_input_9; bash ../post_process.sh; cd ..
+cd sg_input_10; bash ../post_process.sh; cd ..
+cd sg_input_11; bash ../post_process.sh; cd ..
+cd sg_input_13; bash ../post_process.sh; cd ..

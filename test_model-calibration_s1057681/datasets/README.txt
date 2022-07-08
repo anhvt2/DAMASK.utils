2940386f524c86f859a0f3592164c94eb60b45ca
Anh Tran <anh.vt2@gmail.com>
2022-07-08 14:29:26 -0400

    add datasets/ from Hojun Lim

---
+++ test_model-calibration_s1057681/datasets/README.txt
@@ -0,0 +1,10 @@
+
+# Ta
+
+`Ta_polycrystal_SS_HCStack_CLC.txt` from Hojun Lim
+
+# SS304L
+
+`SS304L_EngStress_EngStrain_exp_4A1.txt` from Hojun Lim
+
+
---
+++ test_model-calibration_s1057681/datasets/plotStressStrain.py
@@ -0,0 +1,23 @@
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument("-f", "--f", help='file name with first header row', type=str, required=True)
+args = parser.parse_args()
+
+fileName = args.f
+
+mpl.rcParams['xtick.labelsize'] = 24
+mpl.rcParams['ytick.labelsize'] = 24
+
+x = np.loadtxt(fileName, skiprows=1)
+plt.plot(x[:,0], x[:,1], 'bo', ms=5)
+plt.xlabel(r'Engr. $\varepsilon$ [-]', fontsize=24)
+plt.ylabel(r'Engr. $\sigma$ [MPa]', fontsize=24)
+plt.title(fileName, fontsize=24)
+plt.xlim(left=0)
+plt.ylim(bottom=0)
+plt.show()

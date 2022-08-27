b9b1d0def2c88668d3850d53e9a1a988def9af29
Anh Tran <anh.vt2@gmail.com>
2022-08-27 16:49:36 -0400

    add 40x40x40/numProcessors.dat for Solo test

--- test_model-calibration_Solo/mlmc_SS304L_Template/generateMsDream3d.sh
+++ test_model-calibration_Solo/mlmc_SS304L_Template/generateMsDream3d.sh
@@ -88,11 +88,11 @@ source ~/.bashrc # get geom_check environment variable
 for dimCell in $(cat dimCellList.dat); do
 	cd ${dimCell}x${dimCell}x${dimCell}
 	echo ${dimCell} > dimCell.dat # update dimCell.dat
-
 	cat ../material.config.preamble  | cat - material.config | sponge material.config
 	geom_check single_phase_equiaxed_${dimCell}x${dimCell}x${dimCell}.geom
 	sh ../getDream3dInfo.sh
 	ln -sf ../tension.load
+	cp -fv ../material.config.preamble  .
 	cd ..
 done
 

32db2d2e0cd790fc44d68be12657a4f1eef794cd
Anh Tran <anh.vt2@gmail.com>
2022-08-30 21:18:35 -0400

    finish parse2MaterialConfig.py * verify input and output are correct * bayesOpt inputs are bounded in [0,1]

--- test_model-calibration_Solo/mlmc_SS304L_Template/parse2MaterialConfig.py
+++ test_model-calibration_Solo/mlmc_SS304L_Template/parse2MaterialConfig.py
@@ -15,8 +15,8 @@ import numpy as np
 import os, sys, time, glob, datetime
 
 ## read input
-paramsInput = np.loadtxt('input.dat')
-d = len(paramsInput) # dimensionality
+bayesOpt_input = np.loadtxt('input.dat')
+d = len(bayesOpt_input) # dimensionality
 
 matcfgFile = open('material.config') # use 'materials.config' as the template file
 txtcfg = matcfgFile.readlines()
@@ -25,65 +25,75 @@ matcfgFile.close()
 parentPath = os.getcwd()
 
 ### NOTE: for new case study, modify getDamaskParams() and parseInput()
-def getDamaskParams(sg_input):
-  # translate dakota input from [-1,1] to real bounds imposed by user
-  # [tau0_slip, tausat_slip, h0, n, a_slip]
-  lower_bounds = [0.5e6,  80e6, 200e6,  50, 1]
-  upper_bounds = [3.5e6, 140e6, 280e6, 120, 4]
-  sg_input     = np.array(sg_input)[2:] # ignore 'id' and 'weights' two columns
+def getDamaskParams(bayesOpt_input):
+  lower_bounds = [0.0001,  10, 1,  5e6 ,   5e6, 1e5]
+  upper_bounds = [0.0085, 120, 5, 500e6, 500e6, 9e6]
   lower_bounds = np.array(lower_bounds)
   upper_bounds = np.array(upper_bounds)
-  real_sg_input = lower_bounds + (upper_bounds - lower_bounds) * (sg_input - (-1)) / (+1 - (-1))
-  return real_sg_input
+  matcfg_input = lower_bounds + (upper_bounds - lower_bounds) * (bayesOpt_input - (0)) / (+1 - (0))
+  return matcfg_input
 
-def parseInput(real_sg_input, txtcfg):
+def parseInput(matcfg_input, txtcfg):
   # unpack values and scale to units
-  tau0_slip   = real_sg_input[0]
-  tausat_slip = real_sg_input[1]
-  h0_slipslip = real_sg_input[2] 
-  n           = real_sg_input[3]
-  a_slip      = real_sg_input[4]
+  gdot0_slip    = matcfg_input[0]
+  n_slip        = matcfg_input[1]
+  a_slip        = matcfg_input[2]
+  tau0_slip     = matcfg_input[3] 
+  tausat_slip   = matcfg_input[4]
+  h0_slipslip   = matcfg_input[5]
   parsed_txtcfg = txtcfg # work on a copied version
   # change lines: always add '\n' at the end of the line
-  # parsed_txtcfg[65 - 1] = 'tau0_slip               16.0e6                # per family\n'
-  # parsed_txtcfg[66 - 1] = 'tausat_slip             148.0e6               # per family\n'
-  # parsed_txtcfg[70 - 1] = 'h0_slipslip             2.4e8 # old value: 180e6\n'
-  # parsed_txtcfg[64 - 1] = 'n_slip                  83.3\n'
-  # parsed_txtcfg[67 - 1] = 'a_slip                  2.25\n'
-  parsed_txtcfg[65 - 1] = 'tau0_slip               %.12e                # per family\n' % tau0_slip
-  parsed_txtcfg[66 - 1] = 'tausat_slip             %.12e                # per family\n' % tausat_slip
-  parsed_txtcfg[70 - 1] = 'h0_slipslip             %.12e\n' % h0_slipslip
-  parsed_txtcfg[64 - 1] = 'n_slip                  %.12e\n' % n
-  parsed_txtcfg[67 - 1] = 'a_slip                  %.12e\n' % a_slip
+  # parsed_txtcfg[48 - 1] = 'gdot0_slip              0.001\n'
+  # parsed_txtcfg[49 - 1] = 'n_slip                  83.3\n'
+  # parsed_txtcfg[50 - 1] = 'a_slip                  2.25\n'
+  # parsed_txtcfg[51 - 1] = 'tau0_slip               95.e6\n'
+  # parsed_txtcfg[52 - 1] = 'tausat_slip             222.e6\n'
+  # parsed_txtcfg[53 - 1] = 'h0_slipslip             1.0e6\n'
+  parsed_txtcfg[48 - 1] = 'gdot0_slip              %.12e\n' % gdot0_slip
+  parsed_txtcfg[49 - 1] = 'n_slip                  %.12e\n' % n_slip
+  parsed_txtcfg[50 - 1] = 'a_slip                  %.12e\n' % a_slip
+  parsed_txtcfg[51 - 1] = 'tau0_slip               %.12e\n' % tau0_slip
+  parsed_txtcfg[52 - 1] = 'tausat_slip             %.12e\n' % tausat_slip
+  parsed_txtcfg[53 - 1] = 'h0_slipslip             %.12e\n' % h0_slipslip
   return parsed_txtcfg
 
 
 ### main function
-os.system('rm -rfv sg_input_*') # remove all folders
+matcfg_input = getDamaskParams(bayesOpt_input) # translate from [0,1] to [lower_bounds, upper_bounds]
+np.savetxt('bayesOpt_input.dat'     , bayesOpt_input, fmt='%.16e', delimiter=',') # debug
+np.savetxt('matcfg_input.dat', matcfg_input, fmt='%.16e', delimiter=',') # debug
+parsed_txtcfg = parseInput(matcfg_input, txtcfg)
+f = open('material.config', 'w') # can be 'r', 'w', 'a', 'r+'
+for j in range(len(parsed_txtcfg)):
+  f.write(parsed_txtcfg[j])
+f.close()
 
-for i in range(n):
-  ii = i + 1 # dakota index starts from 1
-  folderName = 'sg_input_%d' % (ii)
-  os.system('mkdir -p %s' % (folderName))
-  localPath = parentPath + '/' + folderName
-  os.chdir(localPath)
-  ## link file from parent directory
-  os.system('ln -sf ../numProcessors.dat .')
-  os.system('ln -sf ../single_phase_equiaxed.geom .')
-  os.system('ln -sf ../numerics.config .')
-  os.system('ln -sf ../tension.load .')
-  os.system('ln -sf ../run_damask.sh .')
-  os.system('ln -sf ../sbatch.damask.solo .')
-  ## write new 'material.config' locally
-  sg_input = paramsInput[i] # get input from dakota
-  real_sg_input = getDamaskParams(sg_input)
-  np.savetxt('sg_input_%d.dat' % ii     , sg_input, fmt='%.16e', delimiter=',') # debug
-  np.savetxt('real_sg_input_%d.dat' % ii, real_sg_input, fmt='%.16e', delimiter=',') # debug
-  parsed_txtcfg = parseInput(real_sg_input, txtcfg)
-  f = open('material.config', 'w') # can be 'r', 'w', 'a', 'r+'
-  for j in range(len(parsed_txtcfg)):
-    f.write(parsed_txtcfg[j])
-  f.close()
-  os.chdir(parentPath)
-  print('Created folder %s' % folderName)
+
+# os.system('rm -rfv bayesOpt_input_*') # remove all folders
+
+# for i in range(n):
+#   ii = i + 1 # dakota index starts from 1
+#   folderName = 'bayesOpt_input_%d' % (ii)
+#   os.system('mkdir -p %s' % (folderName))
+#   localPath = parentPath + '/' + folderName
+#   os.chdir(localPath)
+#   ## link file from parent directory
+#   os.system('ln -sf ../numProcessors.dat .')
+#   os.system('ln -sf ../single_phase_equiaxed.geom .')
+#   os.system('ln -sf ../numerics.config .')
+#   os.system('ln -sf ../tension.load .')
+#   os.system('ln -sf ../run_damask.sh .')
+#   os.system('ln -sf ../sbatch.damask.solo .')
+#   ## write new 'material.config' locally
+#   bayesOpt_input = bayesOpt_input[i] # get input from dakota
+#   matcfg_input = getDamaskParams(bayesOpt_input)
+#   np.savetxt('bayesOpt_input_%d.dat' % ii     , bayesOpt_input, fmt='%.16e', delimiter=',') # debug
+#   np.savetxt('matcfg_input_%d.dat' % ii, matcfg_input, fmt='%.16e', delimiter=',') # debug
+#   parsed_txtcfg = parseInput(matcfg_input, txtcfg)
+#   f = open('material.config', 'w') # can be 'r', 'w', 'a', 'r+'
+#   for j in range(len(parsed_txtcfg)):
+#     f.write(parsed_txtcfg[j])
+#   f.close()
+#   os.chdir(parentPath)
+#   print('Created folder %s' % folderName)
 

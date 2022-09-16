39ff86ef49d7be9964dd5714be1f60df5d311ca1
Anh Tran <anh.vt2@gmail.com>
2022-09-16 00:31:43 -0400

    add negative sign for computeLossFunction * want to minimize loss under maximization settings

--- test_model-calibration_Solo/mlmcSS304L_Iter8/computeLossFunction.py
+++ test_model-calibration_Solo/mlmcSS304L_Iter8/computeLossFunction.py
@@ -111,5 +111,5 @@ if plotDebug:
 f = open('output.dat', 'w') # can be 'r', 'w', 'a', 'r+'
 # f.write('%.8e\n' % (loss_nla / 1e3)) # example: 20097.859541889356 -- scale by a factor of 1e3
 # f.write('%.8e\n' % (loss_nla / 1e3 / max_interp_vareps)) # example: 20097.859541889356 -- scale by a factor of 1e3
-f.write('%.8e\n' % (np.log(loss_nla / max_interp_vareps))) # example: 20097.859541889356 -- scale by a factor of 1e3
+f.write('%.8e\n' % (- np.log(loss_nla / max_interp_vareps))) # example: 20097.859541889356 -- scale by a factor of 1e3
 f.close()

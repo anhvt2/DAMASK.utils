c6ad15079507b7a2fefdaedc55eee25e60637420
Anh Tran <anh.vt2@gmail.com>
2022-09-21 16:12:07 -0600

    add on-the-fly results

---
+++ test_model-calibration_Solo/SS304L_Iter40/check_variances.py
@@ -0,0 +1,55 @@
+import time
+import subprocess
+import numpy as np
+import os
+os.system('rm -fv log.check_variances')
+
+def sample_cpfem(level):
+	cmd = ["python3", "wrapper_DREAM3D-DAMASK.py", "--level=" + str(level)]
+	out = subprocess.run(cmd, stdout=subprocess.PIPE).stdout.decode("utf-8") # execute command and read output
+	lines = out.splitlines() # split output into lines
+	Q = [] # empty array
+	for line in lines: # loop over all lines
+		if "Estimated Yield Stress" in line: # if line containes "Estimated Yield Stress"
+			try:
+				yield_stress = float(line.split()[-2]) # parse the yield stress value to Float64
+				Q.append(yield_stress) # append the value of the yield stress to the array "Q"
+			except:
+				Q.append(None)
+	print(Q)
+	if len(Q) == 2: # safeguard
+		if level == 0:
+			return Q[0], Q[0] # return twice Qf
+		else:
+			return Q[0] - Q[1], Q[0] # return multilevel difference and Qf
+	else:
+		return None, None # safeguard
+
+def check_variances(max_level=3, budget=3600*24*5):
+	buget_per_level = budget/(max_level + 1)
+
+	# for level in range(max_level):
+	# for level in range(max_level - 1, -1, -1): # reverse order
+	for level in [0]: # only sample at level = 0
+		samps_dQ = []
+		samps_Qf = []
+		timer = 0
+		while timer < buget_per_level:
+			t = time.time()
+			dQ, Qf = sample_cpfem(level)
+			if dQ != None and Qf != None: # if both dQ and Qf are not both None
+				timer += time.time() - t
+				samps_dQ.append(dQ)
+				samps_Qf.append(Qf)
+				print("level =", level,
+						", E =", np.mean(samps_Qf),
+						", dE =", np.mean(samps_dQ),
+						", V =", np.var(samps_Qf),
+						", dV =", np.var(samps_dQ),
+						",", len(samps_dQ), "samples, time per sample is", timer/len(samps_dQ))
+				logFile = open('log.check_variances', 'a')
+				logFile.write("level = %d, E = %.8f, dE = %.8f, V = %.8f, dV = %.8f, len(samps_dQ) = %d samples, time per sample is %.8f\n" % (level, np.mean(samps_Qf), np.mean(samps_dQ), np.var(samps_Qf), np.var(samps_dQ), len(samps_dQ), timer/len(samps_dQ)))
+				logFile.close()
+
+if __name__ == "__main__":
+	check_variances()
---
+++ test_model-calibration_Solo/SS304L_Iter76/plotStressStrain.py
@@ -0,0 +1,103 @@
+
+# postResults single_phase_equiaxed_tension.spectralOut --cr f,p
+# filterTable < single_phase_equiaxed_tension.txt --white inc,1_f,1_p > stress_strain.log
+# python3 plotStressStrain.py --StressStrainFile "stress_strain.log"
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import os, sys, datetime
+import argparse
+import pandas as pd
+
+parser = argparse.ArgumentParser(description='')
+parser.add_argument("-StressStrainFile", "--StressStrainFile", default='stress_strain.log', type=str)
+parser.add_argument("-LoadFile", "--LoadFile", default='tension.load', type=str)
+parser.add_argument("-optSaveFig", "--optSaveFig", type=bool, default=False)
+# parser.add_argument("-skiprows", "--skiprows", type=int, default=4) # deprecated
+args = parser.parse_args()
+StressStrainFile = args.StressStrainFile
+LoadFile = args.LoadFile
+# skiprows = args.skiprows # deprecated
+
+def getMetaInfo(StressStrainFile):
+	"""
+	return 
+	(1) number of lines for headers 
+	(2) list of outputs for pandas dataframe
+	"""
+	fileHandler = open(StressStrainFile)
+	txtInStressStrainFile = fileHandler.readlines()
+	fileHandler.close()
+	numLinesHeader = int(txtInStressStrainFile[0].split('\t')[0])
+	fieldsList = txtInStressStrainFile[numLinesHeader].split('\t')
+	for i in range(len(fieldsList)):
+		fieldsList[i] = fieldsList[i].replace('\n', '')
+	print('numLinesHeader = ', numLinesHeader)
+	print('fieldsList = ', fieldsList)
+	return numLinesHeader, fieldsList
+
+def readLoadFile(LoadFile):
+	load_data = np.loadtxt(LoadFile, dtype=str)
+	n_fields = len(load_data)
+	# assume uniaxial:
+	for i in range(n_fields):
+		if load_data[i] == 'Fdot' or load_data[i] == 'fdot':
+			print('Found *Fdot*!')
+			Fdot11 = float(load_data[i+1])
+		if load_data[i] == 'time':
+			print('Found *totalTime*!')
+			totalTime = float(load_data[i+1])
+		if load_data[i] == 'incs':
+			print('Found *totalIncrement*!')
+			totalIncrement = float(load_data[i+1])
+		if load_data[i] == 'freq':
+			print('Found *freq*!')
+			freq = float(load_data[i+1])
+	return Fdot11, totalTime, totalIncrement
+
+mpl.rcParams['xtick.labelsize'] = 24
+mpl.rcParams['ytick.labelsize'] = 24
+
+# d = np.loadtxt(StressStrainFile, skiprows=4)
+numLinesHeader, fieldsList = getMetaInfo(StressStrainFile)
+# d = np.loadtxt(StressStrainFile, skiprows=skiprows)
+d = np.loadtxt(StressStrainFile, skiprows=numLinesHeader+1)
+# df = pd.DataFrame(d, columns=['inc','elem','node','ip','grain','1_pos','2_pos','3_pos','1_f','2_f','3_f','4_f','5_f','6_f','7_f','8_f','9_f','1_p','2_p','3_p','4_p','5_p','6_p','7_p','8_p','9_p'])
+df = pd.DataFrame(d, columns=fieldsList)
+vareps = [1] + list(df['1_f']) # d[:,1] # strain -- pad original
+sigma  = [0] + list(df['1_p']) # d[:,2] # stress -- pad original
+_, uniq_idx = np.unique(np.array(vareps), return_index=True)
+vareps = np.array(vareps)[uniq_idx]
+sigma  = np.array(sigma)[uniq_idx]
+
+fig = plt.figure()
+ax = fig.add_subplot(111)
+
+
+x = (vareps - 1)
+y = sigma / 1e6
+ax.plot(x, y, c='b', marker='o', linestyle=':', markersize=6)
+
+from scipy.interpolate import interp1d
+splineInterp = interp1d(x, y, kind='cubic', fill_value='extrapolate')
+ax.plot(x, splineInterp(x), c='r', marker='^', linestyle='-', markersize=6)
+plt.legend(['true', 'cubic'])
+
+
+plt.xlabel(r'$\varepsilon$ [-]', fontsize=30)
+plt.ylabel(r'$\sigma$ [MPa]', fontsize=30)
+
+if np.all(sigma > -1e-5):
+	plt.ylim(bottom=0)
+
+if np.all((vareps - 1) > -1e-5):
+	plt.xlim(left=0)
+
+ax.xaxis.set_major_formatter(mpl.ticker.FormatStrFormatter('%.4f'))
+
+parentFolderName = os.getcwd().split('/')[-4:-1]
+plt.title('%s' % parentFolderName, fontsize=24)
+
+plt.show()
+

57703cffafd2d9c68876db3363283dcb6bb4a417
Anh Tran <anh.vt2@gmail.com>
2022-09-28 14:11:06 -0600

    update computeLossFucntion.py: rescale by /1e2 warm-restart

--- test_model-calibration_Solo/autoPostprocess.sh
+++ test_model-calibration_Solo/autoPostprocess.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-# for i in $(seq 20); do
-for i in $(seq 10); do
+for i in $(seq 17); do
 	folderName="SS304L_Iter${i}"
 	cd $folderName
 	# cp ../mlmcSS304L_Template/sbatch.damask.solo .
-	cp ../SS304L_Template/computeLossFunction.py . 
+	cp ../SS304L_Template/computeLossFunction.py .
 	python3 computeLossFunction.py --f=8x8x8/ -p 0
-	echo 1 > complete.dat
-	echo 1 > feasible.dat
-	echo 0 > batchID.dat
-	echo 0 > acquisitionScheme.dat
-	cp *_8x8x8.png ..
+	# echo 1 > complete.dat
+	# echo 1 > feasible.dat
+	# echo 0 > batchID.dat
+	# echo 0 > acquisitionScheme.dat
+	# cp *_8x8x8.png ..
 	cd ..
 	echo "done $folderName"; echo; echo;
 done

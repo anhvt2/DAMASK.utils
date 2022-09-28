075c1f6f494f9b288c2557d59441710c85fb9875
Anh Tran <anh.vt2@gmail.com>
2022-09-28 09:57:36 -0600

    update random6d.py for input.dat formatting

--- test_model-calibration_Solo/SS304L_Template/random6d.py
+++ test_model-calibration_Solo/SS304L_Template/random6d.py
@@ -1,2 +1,5 @@
 import numpy as np
-np.savetxt('input.dat', [np.random.rand(6,1)], fmt='%.18e')
+import pandas as pd
+# np.savetxt('input.dat', list(np.random.rand(6,1)), fmt='%.18e')
+# pd.DataFrame(np.random.rand(6,1)).to_csv('input.dat')
+np.random.rand(6,1).tofile('input.dat', sep=',', format='%.18e')

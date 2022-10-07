a0d3b52f6e195080215ce60b6f0318d6f7e1fe07
Anh Tran <anh.vt2@gmail.com>
2022-10-07 15:29:46 -0400

    update plotStressStrain.py; add 1e-9 to avoid numerical error ( log 0 )

--- test_model-calibration_Solo/datasets/plotStressStrain.py
+++ test_model-calibration_Solo/datasets/plotStressStrain.py
@@ -2,6 +2,10 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
+import scipy.ndimage as ndi
+import scipy.fftpack
+import statsmodels.api as sm
+from scipy.interpolate import interp1d
 
 import argparse
 parser = argparse.ArgumentParser()
@@ -9,15 +13,49 @@ parser.add_argument("-f", "--f", help='file name with first header row', type=st
 args = parser.parse_args()
 
 fileName = args.f
+# fileName = 'true_SS304L_EngStress_EngStrain_exp_4A1.dat'
 
 mpl.rcParams['xtick.labelsize'] = 24
 mpl.rcParams['ytick.labelsize'] = 24
 
 x = np.loadtxt(fileName, skiprows=1)
-plt.plot(x[:,0], x[:,1], 'bo', ms=5)
-plt.xlabel(r'$\varepsilon$ [-]', fontsize=24)
-plt.ylabel(r'$\sigma$ [MPa]', fontsize=24)
-plt.title(fileName, fontsize=24)
-plt.xlim(left=0)
-plt.ylim(bottom=0)
+h = x[1,0] - x[0,0] # strain step size
+
+fig, ax = plt.subplots()
+ax.plot(x[:,0], x[:,1], 'bo', ms=5)
+ax.set_xlabel(r'$\varepsilon$ [-]', fontsize=24)
+ax.set_ylabel(r'$\sigma$ [MPa]', fontsize=24, color='blue')
+ax.set_title(fileName, fontsize=24)
+ax.set_xlim(left=0)
+ax.set_ylim(bottom=0)
+
+
+def smooth(y, box_pts):
+    box = np.ones(box_pts)/box_pts
+    y_smooth = np.convolve(y, box, mode='same')
+    return y_smooth
+
+# https://stackoverflow.com/questions/36252434/predicting-on-new-data-using-locally-weighted-regression-loess-lowess
+lowess = sm.nonparametric.lowess
+lowess_data = lowess(x[:,1], x[:,0], frac=1./len(x))
+lowess_x = list(zip(*lowess_data))[0]
+lowess_y = list(zip(*lowess_data))[1]
+f = interp1d(lowess_x, lowess_y, bounds_error=False)
+from scipy.interpolate import interp1d
+vareps_linspace = np.linspace(x[:,0].min(), x[:,0].max(), num=200)
+sigma_linspace = f(vareps_linspace)
+
+ax2 = ax.twinx()
+ax2.plot(vareps_linspace, np.gradient(sigma_linspace), 'rs', ms=2)
+ax2.set_ylabel(r'$\frac{d\sigma}{d\varepsilon}$', fontsize=24, color='red')
+
+plt.figure()
+
+# print(np.log(vareps_linspace))
+plt.plot(np.log(vareps_linspace), np.log(sigma_linspace), 'bo')
+slope, intercept, r, p, se  = scipy.stats.linregress(np.log(vareps_linspace), np.log(sigma_linspace))
+x_ = np.linspace(np.log(vareps_linspace).min(), np.log(vareps_linspace).max(), num=500)
+print(slope)
+plt.plot(x_, slope * x_ + intercept, 'rx')
+
 plt.show()

6b2513a3b0ac791c6694ad556af1dc0c42a37b09
Anh Tran <anh.vt2@gmail.com>
2022-10-07 23:03:02 -0400

    update utils/plotScatterPlotMatrix.py

--- test_model-calibration_Solo/utils/plotScatterPlotMatrix.py
+++ test_model-calibration_Solo/utils/plotScatterPlotMatrix.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 sns.set_theme(style="ticks")
 
-inpData = np.loadtxt('postproc.input.dat', delimiter=',')
+inpData = np.loadtxt('postproc.input.dat', delimiter=',', dtype=float)
 print(inpData.shape)
 
 label_list = []
@@ -15,16 +15,36 @@ for i in range(inpData.shape[1]):
 
 print(label_list)
 
-# df = pd.DataFrame(inpData, columns = [label_list], index=range(inpData.shape[0]))
+df = pd.DataFrame(inpData) # , columns = [label_list]) # , index=range(inpData.shape[0]))
 # sns.pairplot(df[label_list])
-df = pd.DataFrame(inpData)
+# df = pd.DataFrame(inpData)
 # sns.pairplot(df)
+
+# rename column
+for i in range(inpData.shape[1]):
+	df = df.rename(columns={i: 'x%d' % i})
 
 # https://seaborn.pydata.org/examples/pair_grid_with_kde.html
 g = sns.PairGrid(df, diag_sharey=False)
 g.map_upper(sns.scatterplot, s=15)
-g.map_lower(sns.kdeplot)
+g.map_lower(sns.kdeplot, fill=True)
 g.map_diag(sns.kdeplot)
+
+
+# xlabels,ylabels = [],[]
+
+# for ax in g.axes[-1,:]:
+#     xlabel = ax.xaxis.get_label_text()
+#     xlabels.append(xlabel)
+
+# for ax in g.axes[:,0]:
+#     ylabel = ax.yaxis.get_label_text()
+#     ylabels.append(ylabel)
+
+# for i in range(len(xlabels)):
+#     for j in range(len(ylabels)):
+#         g.axes[j,i].xaxis.set_label_text(xlabels[i])
+#         g.axes[j,i].yaxis.set_label_text(ylabels[j])
 
 plt.show()
 

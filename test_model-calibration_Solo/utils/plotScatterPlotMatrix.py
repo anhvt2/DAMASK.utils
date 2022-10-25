b8673471ba94ecb8c9cddb78146313b393463dce
Anh Tran <anh.vt2@gmail.com>
2022-10-25 15:46:20 -0400

    add bounds (0,1) in 2d for utils/plotScatterPlotMatrix.py

--- test_model-calibration_Solo/utils/plotScatterPlotMatrix.py
+++ test_model-calibration_Solo/utils/plotScatterPlotMatrix.py
@@ -27,8 +27,8 @@ for i in range(inpData.shape[1]):
 # https://seaborn.pydata.org/examples/pair_grid_with_kde.html
 g = sns.PairGrid(df, diag_sharey=False)
 g.map_upper(sns.scatterplot, s=15)
-g.map_lower(sns.kdeplot, fill=True)
-g.map_diag(sns.kdeplot)
+g.map_lower(sns.kdeplot, fill=True, clip=((0,1), (1,0)))
+g.map_diag(sns.kdeplot, clip=(0,1))
 
 
 # xlabels,ylabels = [],[]

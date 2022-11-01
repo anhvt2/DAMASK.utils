2d58965bf5616ee158ac45b0f0863b3d9188e49b
Anh Tran <anh.vt2@gmail.com>
2022-11-01 15:02:17 -0600

    addStrainTensors / addCauchy / addMises postproc

--- test_model-calibration_Solo/test/autoRunS1057681.sh
+++ test_model-calibration_Solo/test/autoRunS1057681.sh
@@ -19,12 +19,18 @@ for j in $(seq 10); do # 1 2
 	prefix="sve${j}"
 	for i in 2 4 8 10 16 20; do # 40; do # 80
 		cd ${prefix}_${i}x${i}x${i};
-		# rm -f nohup.out;
-		# nohup mpirun -np $(cat numProcessors.dat) $DAMASK_spectral --load tension.load --geom single_phase_equiaxed_8x8x8.geom 2>&1 &
-		mpirun -np $(cat numProcessors.dat) $DAMASK_spectral --load tension.load --geom single_phase_equiaxed_${i}x${i}x${i}.geom
-		postResults single_phase_equiaxed_${i}x${i}x${i}_tension.spectralOut --cr f,p
+		# # rm -f nohup.out;
+		# # nohup mpirun -np $(cat numProcessors.dat) $DAMASK_spectral --load tension.load --geom single_phase_equiaxed_8x8x8.geom 2>&1 &
+		# mpirun -np $(cat numProcessors.dat) $DAMASK_spectral --load tension.load --geom single_phase_equiaxed_${i}x${i}x${i}.geom
+		# postResults single_phase_equiaxed_${i}x${i}x${i}_tension.spectralOut --cr f,p
+
 		cd postProc/
-		filterTable < single_phase_equiaxed_${i}x${i}x${i}_tension.txt --white inc,1_f,1_p > stress_strain.log
+
+		# filterTable < single_phase_equiaxed_${i}x${i}x${i}_tension.txt --white inc,1_f,1_p > stress_strain.log
+		addStrainTensors single_phase_equiaxed_${i}x${i}x${i}_tension.txt --left --logarithmic
+		addCauchy single_phase_equiaxed_${i}x${i}x${i}_tension.txt
+		addMises single_phase_equiaxed_${i}x${i}x${i}_tension.txt --strain 'ln(V)' --stress Cauchy
+
 		cd ..
 		cd ..
 	done

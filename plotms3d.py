5bb7ae54a3c8bca6f80a6be693d08197b6b31899
Anh Tran <anh.vt2@gmail.com>
2022-11-01 22:07:06 -0400

    automate for all *.vtr files

--- plotms3d.py
+++ plotms3d.py
@@ -2,26 +2,28 @@
 
 import pyvista
 import matplotlib.pyplot as plt
+import glob, os
 # cmap = plt.cm.get_cmap("viridis", 5)
 # https://predictablynoisy.com/matplotlib/gallery/color/colormap_reference.html#sphx-glr-gallery-color-colormap-reference-py
 # https://matplotlib.org/stable/tutorials/colors/colormaps.html
 cmap = plt.cm.get_cmap('coolwarm')
 
-filename = 'single_phase_equiaxed_8x8x8.vtr'
-reader = pyvista.get_reader(filename)
-msMesh = reader.read()
-ms = msMesh.get_array('microstructure')
-msMesh.cell_data['microstructure']
-msMesh.set_active_scalars('microstructure', preference='cell')
+# filename = 'single_phase_equiaxed_8x8x8.vtr'
+for filename in glob.glob('*.vtr'): # screenshot for all *.vtr files
+	reader = pyvista.get_reader(filename)
+	msMesh = reader.read()
+	ms = msMesh.get_array('microstructure')
+	msMesh.cell_data['microstructure']
+	msMesh.set_active_scalars('microstructure', preference='cell')
 
-# pl = pyvista.Plotter()
-pl = pyvista.Plotter(off_screen=True)
-pl.add_mesh(msMesh, show_edges=True, line_width=5, cmap=cmap)
-pl.background_color = "white"
-pl.remove_scalar_bar()
-# pl.show(screenshot='%s.png' % filename.split('.')[0])
-# pl.show()
-pl.screenshot('%s.png' % filename.split('.')[0])
-# pl.close()
+	# pl = pyvista.Plotter()
+	pl = pyvista.Plotter(off_screen=True)
+	pl.add_mesh(msMesh, show_edges=True, line_width=1, cmap=cmap)
+	pl.background_color = "white"
+	pl.remove_scalar_bar()
+	# pl.show(screenshot='%s.png' % filename.split('.')[0])
+	# pl.show()
+	pl.screenshot('%s.png' % filename.split('.')[0])
+	# pl.close()
 
 

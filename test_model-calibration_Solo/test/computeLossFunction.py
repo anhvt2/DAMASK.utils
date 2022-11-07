6907c032039bfe782b93ea4b87681c941194ff9f
Anh Tran <anh.vt2@gmail.com>
2022-11-07 11:51:11 -0500

    update sbatch.damask.solo

--- test_model-calibration_Solo/test/computeLossFunction.py
+++ test_model-calibration_Solo/test/computeLossFunction.py
@@ -54,10 +54,13 @@ compData = compData[~np.isinf(compData[:,-1]),:]
 
 # df = pd.DataFrame(compData, columns=['inc','elem','node','ip','grain','1_pos','2_pos','3_pos','1_f','2_f','3_f','4_f','5_f','6_f','7_f','8_f','9_f','1_p','2_p','3_p','4_p','5_p','6_p','7_p','8_p','9_p'])
 df = pd.DataFrame(compData, columns=fieldsList)
-comp_vareps = [1] + list(df['1_f']) # d[:,1] # strain -- pad original
-comp_sigma  = [0] + list(df['1_p']) # d[:,2] # stress -- pad original
+# comp_vareps = [1] + list(df['1_f']) # d[:,1] # strain -- pad original
+# comp_sigma  = [0] + list(df['1_p']) # d[:,2] # stress -- pad original
+vareps = list(df['Mises(ln(V))'])  # strain -- pad original
+sigma  = list(df['Mises(Cauchy)']) # stress -- pad original
 _, uniq_idx = np.unique(np.array(comp_vareps), return_index=True)
-comp_vareps = np.array(comp_vareps)[uniq_idx] - 1 # start at vareps = 0
+# comp_vareps = np.array(comp_vareps)[uniq_idx] - 1 # start at vareps = 0: ['1_f', '1_p']
+comp_vareps = np.array(comp_vareps)[uniq_idx] # start at vareps = 0: ['Mises(ln(V))', 'Mises(Cauchy)']
 comp_sigma  = np.array(comp_sigma)[uniq_idx]
 
 

f7607776b0aea4e40e4f743b846b2e9f1c10da5f
Anh Tran <anh.vt2@gmail.com>
2022-11-22 21:53:09 -0500

    add build-damask.sh for DAMASK-3

---
+++ build-damask.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# damaskSrc="${damaskSrc}"
+damaskSrc="DAMASK"
+reset;
+echo "Source directory: ${damaskSrc}"
+
+cmake -S ${damaskSrc} -B build-grid -DDAMASK_SOLVER=grid
+cmake --build build-grid --target install
+cmake -S ${damaskSrc} -B build-mesh -DDAMASK_SOLVER=mesh
+cmake --build build-mesh --target install

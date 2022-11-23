fcf9dd76e4f7d6859e4d51874ed7486fd64dcd7c
Anh Tran <anh.vt2@gmail.com>
2022-11-23 12:02:12 -0500

    complete petsc-3.18.1 built

--- build-damask-3.sh
+++ build-damask-3.sh
@@ -1,20 +1,30 @@
 #!/bin/bash
 
 ### PETSc-configure/build/install
+# successfully built on Solo with modules:
+#   1) cmake/3.22.3   2) mkl/16.0   3) fftw/2.1   4) openmpi-gnu/4.0   5) gnu/10.2.1
 
-sudo ./configure \
-    --prefix=/usr/local/petsc-3.18.1 \
-    --with-fc=gfortran \
-    --with-cc=gcc \
-    --with-cxx=g++ \
-    --download-mpich \
-    --download-fftw \
-    --download-hdf5 \
-    --download-hdf5-fortran-bindings=1 \
-    --download-zlib \
-    --with-mpi-f90module-visibility=0 \
-    PETSC_DIR=$(pwd)
+# unset PETSC_ARCH
+# unset PETSC_DIR
+# sudo ./configure \
+#     --prefix=/usr/local/petsc-3.18.1 \
+#     --with-fc=gfortran \
+#     --with-cc=gcc \
+#     --with-cxx=g++ \
+#     --download-mpich \
+#     --download-fftw \
+#     --download-hdf5 \
+#     --download-hdf5-fortran-bindings=1 \
+#     --download-zlib \
+#     --with-mpi-f90module-visibility=0 \
+#     PETSC_DIR=$(pwd) PETSC_ARCH=arch-linux-c-debug
 
+# sudo make PETSC_DIR=/home/anhvt89/Documents/DAMASK/petsc/petsc-3.18.1 PETSC_ARCH=arch-linux-c-debug all
+# sudo make PETSC_DIR=/home/anhvt89/Documents/DAMASK/petsc/petsc-3.18.1 PETSC_ARCH=arch-linux-c-debug install
+# export PETSC_DIR=/usr/local/petsc-3.18.1/
+# cd $PETSC_DIR
+# ln -sf /home/anhvt89/Documents/DAMASK/petsc/petsc-3.18.1/arch-linux-c-debug . 
+# cd - 
 
 ### build DAMASK
 

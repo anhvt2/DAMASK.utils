70ec183fa0d4d5bc8aa80f75c74dba4dbf25c38b
Anh Tran <anh.vt2@gmail.com>
2023-03-17 13:23:54 -0400

    update run_damask.sh

--- test_MLMC_multiQoIs_Mg/template/run_damask.sh
+++ test_MLMC_multiQoIs_Mg/template/run_damask.sh
@@ -145,19 +145,22 @@ if [ -d "postProc" ]; then
 	filterTable < ${filePrefix}.txt --white inc,'Mises(ln(V))','Mises(Cauchy)' > stress_strain.log
 
 	cp ../tension.load . 
-	# check $1 argument in running this script, i.e. bash run_damask.sh $1
-	if [[ $1 == "YieldStress" ]]; then 
-		python3 ../computeYieldStress.py
-	elif [[ $1 == "YoungModulus" ]]; then
-		python3 ../computeYoungModulus.py
-	else
-		echo "run_damask_2.0.3.sh: \$1 argument is not detected in run_damask_2.0.3.sh"
-	fi
+	python3 ../computeCollatedStress.py
 
-	if [ -f "output.dat" ]; then
-		echo 1 > ../log.feasible
-		# needed in wrapper_DREAM3D-DAMASK.py
-	fi
+	# # check $1 argument in running this script, i.e. bash run_damask.sh $1
+	# if [[ $1 == "YieldStress" ]]; then 
+	# 	python3 ../computeYieldStress.py
+	# elif [[ $1 == "YoungModulus" ]]; then
+	# 	python3 ../computeYoungModulus.py
+	# else
+	# 	echo "run_damask_2.0.3.sh: \$1 argument is not detected in run_damask_2.0.3.sh"
+	# fi
+
+	# if [ -f "output.dat" ]; then
+	# 	echo 1 > ../log.feasible
+	# 	# needed in wrapper_DREAM3D-DAMASK.py
+	# fi
+
 	cd ..
 else
 	echo "Simulation does not converge!!!"

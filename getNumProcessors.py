7452898afe8594898dafada9dce333c81c65446f
Anh Tran <anh.vt2@gmail.com>
2023-03-17 13:42:20 -0400

    add getNumProcessors.py: automatically get number of processors

---
+++ getNumProcessors.py
@@ -0,0 +1,18 @@
+
+import numpy as np
+import os, glob
+from natsort import natsorted, ns
+
+currentPath = os.getcwd()
+
+print('Current path = %s' % currentPath)
+for folderName in natsorted(glob.glob('*x*x*')):
+	# print(folderName)
+	dimX, dimY, dimZ = folderName.split('x')
+	dimX, dimY, dimZ = int(dimX), int(dimY), int(dimZ)
+	numProcessors = int(np.floor(dimZ / 4.))
+	if numProcessors <= 0:
+		numProcessors += 1
+	print('Assigning %d processors to %s' % (numProcessors, folderName))
+	np.savetxt(folderName + '/numProcessors.dat', [numProcessors], fmt='%d')
+

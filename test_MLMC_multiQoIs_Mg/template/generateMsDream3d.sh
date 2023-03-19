ec9f3cffdaf1e787089b425c78a861a1f69ddfcf
Anh Tran <anh.vt2@gmail.com>
2023-03-18 20:45:42 -0400

    add a mandatory numerics.config

--- test_MLMC_multiQoIs_Mg/template/generateMsDream3d.sh
+++ test_MLMC_multiQoIs_Mg/template/generateMsDream3d.sh
@@ -98,7 +98,8 @@ source ~/.bashrc # get geom_check environment variable
 for dimCell in $(cat dimCellList.dat); do
 	cd ${dimCell}x${dimCell}x${dimCell}
 	echo ${dimCell} > dimCell.dat # update dimCell.dat
-	ln -sf ../tension.load . 
+	ln -sf ../tension.load .
+	ln -sf ../numerics.config .
 	cat ../material.config.preamble  | cat - material.config | sponge material.config
 	geom_check MgRve_${dimCell}x${dimCell}x${dimCell}.geom
 	sh ../getDream3dInfo.sh

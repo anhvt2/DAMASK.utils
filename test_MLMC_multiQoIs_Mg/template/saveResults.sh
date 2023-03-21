097e3d74001dd28f59bbe64ca116997ccdb6bfa8
Anh Tran <anh.vt2@gmail.com>
2023-03-21 02:36:09 -0400

    debug saveResults.sh

--- test_MLMC_multiQoIs_Mg/template/saveResults.sh
+++ test_MLMC_multiQoIs_Mg/template/saveResults.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 timeStamp=$(date +%y-%m-%d-%H-%M-%S)
-for folderName in $(ls -1dv */); do
-	mv ${folderName} ${folderName}-${timeStamp}
-done
+# for folderName in $(ls -1dv */); do
+	# mv ${folderName} ${folderName}-${timeStamp}
+# done
 
+for dimCell in $(cat dimCellList.dat); do
+	mv ${dimCell}x${dimCell}x${dimCell} ${dimCell}x${dimCell}x${dimCell}-${timeStamp}
+done

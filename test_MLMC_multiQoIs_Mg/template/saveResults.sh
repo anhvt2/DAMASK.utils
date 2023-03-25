497fe499f1e5f38c795e09a4b5c1db48fb9385ec
Anh Tran <anh.vt2@gmail.com>
2023-03-24 21:12:34 -0400

    save results to bak/ and not interfere with current run

--- test_MLMC_multiQoIs_Mg/template/saveResults.sh
+++ test_MLMC_multiQoIs_Mg/template/saveResults.sh
@@ -1,10 +1,12 @@
 #!/bin/bash
 
+mkdir -p bak/
+
 timeStamp=$(date +%y-%m-%d-%H-%M-%S)
 # for folderName in $(ls -1dv */); do
 	# mv ${folderName} ${folderName}-${timeStamp}
 # done
 
 for dimCell in $(cat dimCellList.dat); do
-	mv ${dimCell}x${dimCell}x${dimCell} ${dimCell}x${dimCell}x${dimCell}-${timeStamp}
+	mv ${dimCell}x${dimCell}x${dimCell} bak/${dimCell}x${dimCell}x${dimCell}-${timeStamp}
 done

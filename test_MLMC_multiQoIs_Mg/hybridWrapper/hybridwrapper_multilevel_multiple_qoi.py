24f25b400c98f6b11c321698b31aec335bbd95ca
Anh Tran <anh.vt2@gmail.com>
2023-04-12 10:57:24 -0400

    recycle info: lookup if possible; if not - run DAMASK

--- test_MLMC_multiQoIs_Mg/hybridWrapper/hybridwrapper_multilevel_multiple_qoi.py
+++ test_MLMC_multiQoIs_Mg/hybridWrapper/hybridwrapper_multilevel_multiple_qoi.py
@@ -22,39 +22,43 @@ nb_of_qoi = int(args.nb_of_qoi) # currently not being used
 d = np.loadtxt('MultilevelEstimators-multiQoIs.dat', skiprows=1, delimiter=',')
 
 ### lookup data
-query_index = np.where(d[:,0] == level)[0][0] # return the first hit results
+query = np.where(d[:,0] == level)[0]
+if len(query) > 0: # if there are results, then simply look up
 
-if level == 0:
-	results = d[ query_index ]
-	stress_only_results = results[1:]
-else:
-	results = d[ [query_index, query_index + 1] ] # return level and (level - 1)
-	stress_only_results = results[:,1:]
+	query_index = query[0] # return the first hit results
 
-### print results w/ identical interface
+	if level == 0:
+		results = d[ query_index ]
+		stress_only_results = results[1:]
+	else:
+		results = d[ [query_index, query_index + 1] ] # return level and (level - 1)
+		stress_only_results = results[:,1:]
 
-if level == 0:
-	vmStress2str = np.array(stress_only_results, dtype=str)
-	str2print = ', '.join(vmStress2str) # construct a string to print on screen
-	print("Collocated von Mises stresses at %d is %s " % (level, str2print))
-else:
-	# (level)
-	vmStress2str_current_level = np.array(stress_only_results[0], dtype=str)
-	str2print_current_level = ', '.join(vmStress2str_current_level)
-	# (level - 1)
-	vmStress2str_below_level = np.array(stress_only_results[1], dtype=str)
-	str2print_below_level = ', '.join(vmStress2str_below_level) # construct a string to print on screen
-	print("Collocated von Mises stresses at %d is %s " % (level, str2print_current_level))
-	print("Collocated von Mises stresses at %d is %s " % (level - 1, str2print_below_level))
+	### print results w/ identical interface
 
-### reduce results
-if level == 0:
-	reduced_d = np.delete(d, (query_index), axis=0) # remove the corresponding results after query
-else:
-	reduced_d = np.delete(d, (query_index, query_index + 1), axis=0) # remove the corresponding results after query
+	if level == 0:
+		vmStress2str = np.array(stress_only_results, dtype=str)
+		str2print = ', '.join(vmStress2str) # construct a string to print on screen
+		print("Collocated von Mises stresses at %d is %s " % (level, str2print))
+	else:
+		# (level)
+		vmStress2str_current_level = np.array(stress_only_results[0], dtype=str)
+		str2print_current_level = ', '.join(vmStress2str_current_level)
+		# (level - 1)
+		vmStress2str_below_level = np.array(stress_only_results[1], dtype=str)
+		str2print_below_level = ', '.join(vmStress2str_below_level) # construct a string to print on screen
+		print("Collocated von Mises stresses at %d is %s " % (level, str2print_current_level))
+		print("Collocated von Mises stresses at %d is %s " % (level - 1, str2print_below_level))
 
-### save reduced dataset after eliminating appropriate rows
-np.savetxt("MultilevelEstimators-multiQoIs.dat", reduced_d, delimiter=",", header="level, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9", fmt="%d, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e")
+	### reduce results
+	if level == 0:
+		reduced_d = np.delete(d, (query_index), axis=0) # remove the corresponding results after query
+	else:
+		reduced_d = np.delete(d, (query_index, query_index + 1), axis=0) # remove the corresponding results after query
 
+	### save reduced dataset after eliminating appropriate rows
+	np.savetxt("MultilevelEstimators-multiQoIs.dat", reduced_d, delimiter=",", header="level, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9", fmt="%d, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e")
 
+else: # if there is no results, then run DAMASK
+	os.system('python3 wrapperMLMC-DREAM3D-DAMASK.py --level=%d --nb_of_qoi 10' % (level))
 

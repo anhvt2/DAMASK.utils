620c7315e88864f48246e189ca0c717f3e14ab2e
Anh Tran <anh.vt2@gmail.com>
2023-04-13 11:58:03 -0400

    add 2 removal conditions
    1. non-monotonic stress-strain
    2. stress out of range (> 1e3 threshold)

--- test_MLMC_multiQoIs_Mg/fakeWrapper/cleanseDataset.py
+++ test_MLMC_multiQoIs_Mg/fakeWrapper/cleanseDataset.py
@@ -22,6 +22,25 @@ for i in range(len(txt)):
 
 d = np.array(d, dtype=float)
 
+del_idx = []
+for i in range(d.shape[0]):
+	# removal condition: out of range stress > threshold
+	if np.any(d[i,:] > 5e2):
+		# print(i)
+		del_idx.append(i)
+		if d[i, 0] - d[i+1, 0] == 1:
+			# print(i+1)
+			del_idx.append(i+1)
+		elif d[i-1, 0] - d[i, 0] == 1:
+			# print(i-1)
+			del_idx.append(i-1)
+	# removal condition: non-monotonic stress
+	if np.any(np.diff(d[i, 1:]) < 0):
+		# print(i)
+		del_idx.append(i)
+
+d = np.delete(d, del_idx, axis=0)
+
 # save dataset
 np.savetxt("MultilevelEstimators-multiQoIs.dat", d, delimiter=",", header="level, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9", fmt="%d, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e")
 

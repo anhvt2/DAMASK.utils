07cf1c75c733e40190e3302a47fb33c65d5fadd2
Anh Tran <anh.vt2@gmail.com>
2023-04-18 09:29:51 -0600

    run more at level 2

--- test_MLMC_multiQoIs_Mg/test/run_more_samples_specific_level.sh
+++ test_MLMC_multiQoIs_Mg/test/run_more_samples_specific_level.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for i in $(seq 10); do
-	python3 wrapper_multilevel_multiple_qoi.py --level=4 --nb_of_qoi=10
+for i in $(seq 200); do
+	python3 wrapper_multilevel_multiple_qoi.py --level=2 --nb_of_qoi=10
 done
 

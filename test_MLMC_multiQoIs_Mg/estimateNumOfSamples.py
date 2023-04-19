a30c4f7a86ec22b6b96ca3e64f4c94701f8f9495
Anh Tran <anh.vt2@gmail.com>
2023-04-19 15:14:32 -0400

    print total samples

--- test_MLMC_multiQoIs_Mg/estimateNumOfSamples.py
+++ test_MLMC_multiQoIs_Mg/estimateNumOfSamples.py
@@ -9,12 +9,14 @@ num_level = len(cost_per_level)
 parser = argparse.ArgumentParser()
 parser.add_argument("-ms", "--max_sample", type=int) # desired number of samples at the highest level of fidelity
 
-
-
 args = parser.parse_args()
 max_sample = int(args.max_sample)
 
+num_samples = np.array(max_sample * np.max(cost_per_level) / cost_per_level, dtype=int)
 
 for i in range(num_level):
-	print(f"Need {int( max_sample * cost_per_level[-1] / cost_per_level[i])} samples at level {i}")
+	# print(f"Need {int( max_sample * cost_per_level[-1] / cost_per_level[i])} samples at level {i}")
+	print(f"Need {num_samples[i]} samples at level {i}.")
+
+print(f"Need {np.sum(num_samples)} total samples.")
 

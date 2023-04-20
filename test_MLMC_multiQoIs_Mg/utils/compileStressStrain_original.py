5f8c1925f7b75fddf34e45769215d41868d6e9e3
Anh Tran <anh.vt2@gmail.com>
2023-04-20 09:48:35 -0400

    debug, polish

--- test_MLMC_multiQoIs_Mg/utils/compileStressStrain_original.py
+++ test_MLMC_multiQoIs_Mg/utils/compileStressStrain_original.py
@@ -55,44 +55,55 @@ def getMetaInfo(StressStrainFile):
 plt.figure()
 
 dimCellList = [2, 4, 8, 16, 32]
-colorList = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
-alphaList = [0.4, 0.5, 0.6, 0.7, 0.8]
+# colorList = ['tab:blue', 'tab:orange', 'tab:green', 'tab:red', 'tab:purple']
+colorList = ["#5f9fff",
+"#cdcb57",
+"#ff8269",
+"#006b38",
+"#905300"]
+alphaList = np.linspace(0.7,0.9,5)
+linestyleList = ['dotted', 'dotted', 'dashdot', 'dashed', 'solid']
+markersizeList = np.linspace(4,6,num=5)
 validLegendFileList = ['', '', '', '', ''] # collect last valid file for legend plotting purpose
 
 for i in range(len(dimCellList)):
 	dimCell = dimCellList[i]
 	num_obs = len(glob.glob('%sx%sx%s*stress_strain.log' % (dimCell, dimCell, dimCell)))
 	print(f"Found {num_obs} observations at {dimCell}x{dimCell}x{dimCell}")
-
+	#
 	for fileName in glob.glob('%sx%sx%s*stress_strain.log' % (dimCell, dimCell, dimCell)):
 		strain, stress = getTrueStressStrain(fileName)
-
 		#
-		if np.any(stress > 3e2) or np.any(np.diff(stress) < 0) or np.max(strain) < 0.1:
+		if np.any(stress > 3e2) or np.any(np.diff(stress) < 0) or np.max(strain) < 0.08 or np.any(stress) < 0:
+		# if np.max(strain) < 0.08:
 			print(f"{fileName} is not usable.")
 		else:
 			interp_strain = np.linspace(0, 0.12, num=201)
 			collocated_strain = np.linspace(0, 0.10, num=11)
+			
+			# choose one interpolator
 			# splineInterp = interp1d(strain, stress, kind='cubic', fill_value='extrapolate')
 			splineInterp = PchipInterpolator(strain, stress)
 			# splineInterp = interp1d(strain, stress, kind='cubic', fill_value=('NaN','NaN'))
+
 			interp_stress = splineInterp(interp_strain)
 			collocated_stress = splineInterp(collocated_strain)
-			plt.plot(interp_strain, interp_stress, color=colorList[i], alpha=alphaList[i], linewidth=i)
-			plt.plot(collocated_strain, collocated_stress, 'rs', linestyle='None')
-			plt.plot(strain, stress, 'bo', linestyle='None')
-			# plt.plot(strain, stress, color=, alpha=alphaList[i], marker='o', linewidth=i)
+			plt.plot(interp_strain, interp_stress, color=colorList[i], alpha=alphaList[i], linewidth=i+1, linestyle=linestyleList[i], markersize=markersizeList[i])
+			plt.plot(collocated_strain, collocated_stress, marker='x', color='r', linestyle='None', markersize=markersizeList[i])
+			# plt.plot(strain, stress, 'bo', linestyle='None')
+			plt.plot(strain, stress, color=colorList[i], alpha=alphaList[i], marker='o', linewidth=0, linestyle=linestyleList[i])
+			print(f"{fileName} is usable.")
 			validLegendFileList[i] = fileName
 
 # plot for legend
-plt.plot(strain, stress, 'bo', linestyle='None', label='observations')
-plt.plot(collocated_strain, collocated_stress, 'rs', linestyle='None', label='collocated observations')
+plt.plot(strain, stress, color='k', marker='o', markerfacecolor='None', linestyle='None', label='observations')
+plt.plot(collocated_strain, collocated_stress, color='r', marker='x', markerfacecolor='None', linestyle='None', label='QoIs')
 # print(validLegendFileList)
 for i in range(len(dimCellList)):
 	dimCell = dimCellList[i]
 	fileName = validLegendFileList[i]
 	strain, stress = getTrueStressStrain(fileName)
-	plt.plot(strain, stress, marker='None', color=colorList[i], label='%sx%sx%s' % (dimCell, dimCell, dimCell))
+	plt.plot(strain, stress, marker='None', color=colorList[i], linestyle=linestyleList[i], label='%sx%sx%s' % (dimCell, dimCell, dimCell))
 
 
 # custom plot
@@ -102,5 +113,7 @@ plt.legend(loc='best', fontsize=24, frameon=False, markerscale=2)
 plt.xlabel(r'$\varepsilon_{vM}$ [-]', fontsize=24)
 plt.ylabel(r'$\sigma_{vM}$ [MPa]' , fontsize=24)
 plt.title(r'Compilation of equivalent $\sigma-\varepsilon$ curves for Mg', fontsize=24)
+from matplotlib.ticker import StrMethodFormatter
+plt.gca().xaxis.set_major_formatter(StrMethodFormatter('{x:,.2f}')) # 2 decimal places
 plt.show()
 

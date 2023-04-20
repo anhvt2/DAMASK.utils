43e864372ccdb83f1f20b74b47b2a2e11093039b
Anh Tran <anh.vt2@gmail.com>
2023-04-20 15:30:16 -0400

    aggregate log.MultilevelEstimators-multiQoIs

--- test_MLMC_multiQoIs_Mg/utils/collect_data.sh
+++ test_MLMC_multiQoIs_Mg/utils/collect_data.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
+rm -v log.MultilevelEstimators-multiQoIs
+
 for folderName in $(find . -name 'postProc'); do
 	shortFolderName=$(echo $folderName | cut -d/ -f4)
 	cp $folderName/output.dat ./$shortFolderName-output.dat
 	cp $folderName/stress_strain.log ./$shortFolderName-stress_strain.log
+
+	cat $folderName/../../../log.MultilevelEstimators-multiQoIs >> log.MultilevelEstimators-multiQoIs
 	# echo $shortFolderName
 done
 

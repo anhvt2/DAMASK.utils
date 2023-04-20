c628bd05054604a606ae407aa3aa593654fea0b4
Anh Tran <anh.vt2@gmail.com>
2023-04-19 21:25:25 -0400

    collect stress_strain.log

--- test_MLMC_multiQoIs_Mg/utils/compile_stress_strain_logs.sh
+++ test_MLMC_multiQoIs_Mg/utils/compile_stress_strain_logs.sh
@@ -3,6 +3,7 @@
 for folderName in $(find . -name 'postProc'); do
 	shortFolderName=$(echo $folderName | cut -d/ -f2)
 	cp $shortFolderName/postProc/output.dat ./$shortFolderName-output.dat
+	cp $shortFolderName/postProc/stress_strain.log ./$shortFolderName-stress_strain.log
 	# echo $shortFolderName
 done
 

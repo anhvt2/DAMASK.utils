449b1b369d209791831e9096e1bb5662e78794bf
Anh Tran <anh.vt2@gmail.com>
2023-04-21 20:50:23 -0400

    pack data in tarballs

--- test_MLMC_multiQoIs_Mg/utils/collect_data.sh
+++ test_MLMC_multiQoIs_Mg/utils/collect_data.sh
@@ -11,3 +11,13 @@ for folderName in $(find . -name 'postProc'); do
 	# echo $shortFolderName
 done
 
+# pack into 1 single tarball
+
+tag=$(basename $(pwd))
+tar cvzf ${tag}.stress_strain.log.tar.gz *stress_strain.log
+rm *stress_strain.log
+
+tar cvzf ${tag}.output.dat.tar.gz *output.dat
+rm *output.dat
+
+cp log.MultilevelEstimators-multiQoIs ../${tag}.log.MultilevelEstimators-multiQoIs

e6b95d150c35e0d1743186cff08364ed68cbe203
Anh Tran <anh.vt2@gmail.com>
2023-04-24 22:01:38 -0400

    add development - note: full of bugs

--- test_MLMC_multiQoIs_Mg/fakeWrapper/cleanseDataset.py
+++ test_MLMC_multiQoIs_Mg/fakeWrapper/cleanseDataset.py
@@ -33,7 +33,7 @@ def import_dataset(datasetFileName):
 	tossaway_idx = []
 	# print(d.shape[0]) # debug
 	ii = 0 # debug
-	while ii < d.shape[0] - 1:
+	while ii < d.shape[0]-1:
 		# print(ii)
 		if d[ii,0] - d[ii+1,0] == 1:
 			# print(f"Index {ii} valid in {datasetFileName}. Skip index {ii+1}.") # debug
@@ -46,25 +46,12 @@ def import_dataset(datasetFileName):
 				# print(f"{d[ii]}, {d[ii+1]}")
 				# print(txt[ii])
 				tossaway_idx.append(ii)
-			## deprecated: move to later
-			# if np.any(np.isnan(d[ii,:])):
-			# 	if d[ii,0] == 0:
-			# 		print(f"Index {ii} is not valid in {datasetFileName}: NaN found. -- Discard.")
-			# 		tossaway_idx.append(ii)
-			# 		print(f"{ii}: {np.any(np.isnan(d[ii,:]))}") # debug
-			# 	else:
-			# 		tossaway_idx.append(ii)
-			# 		print(f"Index {ii} is not valid in {datasetFileName}: NaN found. Discard.")
-			# 		if d[ii,0] - d[ii+1,0] == 1:
-			# 			tossaway_idx.append(ii+1)
-			# 			print(f"Index {ii+1} is not valid in {datasetFileName}: NaN found in previous level. -- Discard.")
-			# 			ii += 1
 		ii += 1
 
 	tossaway_idx = np.sort(np.unique(np.array(tossaway_idx)))
 
 	# print(len(txt))
-	for i in range(len(tossaway_idx) - 1, -1, -1):
+	for i in range(len(tossaway_idx)-1, -1, -1):
 		txt.pop(tossaway_idx[i])
 	# import
 	logFile = open('log.MultilevelEstimators-multiQoIs', 'a+')
@@ -82,44 +69,71 @@ for i in range(1,5):
 # import_dataset('../hpc-run-5.log.MultilevelEstimators-multiQoIs') # nan - debug
 
 # read dataset
-logFile = open('log.MultilevelEstimators-multiQoIs')
-txt = logFile.readlines()
-logFile.close()
 
-d = []
+# --- BEGIN DEPRECATED
+# logFile = open('log.MultilevelEstimators-multiQoIs')
+# txt = logFile.readlines()
+# logFile.close()
 
-# cleanse dataset
-for i in range(len(txt)):
-	txt[i] = txt[i].replace('Collocated von Mises stresses at ', '')
-	txt[i] = txt[i].replace(' is ', ',') # replace with a comma
-	txt[i] = txt[i].replace('\n', '') 
-	tmp_list = txt[i].split(',')
-	d += [tmp_list]
+# d = []
 
-d = np.array(d, dtype=float)
+# # cleanse dataset
+# for i in range(len(txt)):
+# 	txt[i] = txt[i].replace('Collocated von Mises stresses at ', '')
+# 	txt[i] = txt[i].replace(' is ', ',') # replace with a comma
+# 	txt[i] = txt[i].replace('\n', '') 
+# 	tmp_list = txt[i].split(',')
+# 	d += [tmp_list]
+# --- END DEPRECATED
+
+# d = np.array(d, dtype=float)
+d = np.loadtxt('log.MultilevelEstimators-multiQoIs', delimiter=',')
 num_levels = int(np.max(d[:,0]) - np.min(d[:,0]) + 1)
 
 del_idx = []
-for i in range(d.shape[0]):
+i = 0
+while i < d.shape[0]-1:
 	# removal condition: out of range stress > threshold
 	if np.any(d[i,:] > 5e2):
-		# print(i)
 		del_idx.append(i)
-		if d[i, 0] - d[i+1, 0] == 1:
-			# print(i+1)
-			del_idx.append(i+1)
-		elif d[i-1, 0] - d[i, 0] == 1:
-			# print(i-1)
-			del_idx.append(i-1)
+		i += 1
+		if d[i, 0] != 0 and d[i, 0] - d[i+1, 0] == 1:
+			del_idx.append(i+1) # delete forward
+			i += 1
+			if d[i-1, 0] - d[i,0] == 1:
+				del_idx.append(i-1) # delete backward
+	else:
+		i += 1
+
+i = 0
+while i < d.shape[0]-1:
 	# removal condition: non-monotonic stress
 	if np.any(np.diff(d[i, 1:]) < 0):
-		# print(i)
 		del_idx.append(i)
+		i += 1
+		if d[i, 0] != 0 and d[i, 0] - d[i+1, 0] == 1:
+			del_idx.append(i+1) # delete forward
+			i += 1
+			if d[i-1, 0] - d[i,0] == 1:
+				del_idx.append(i-1) # delete backward
+	else:
+		i += 1
 
-for i in range(d.shape[0]):
+# # for i in range(d.shape[0]):
+i = 0
+while i < d.shape[0]-1:
 	# removal condition: nan
 	if np.any(np.isnan(d[i, 1:])):
 		del_idx.append(i)
+		i += 1
+		if d[i, 0] != 0 and d[i, 0] - d[i+1, 0] == 1:
+			del_idx.append(i+1) # delete forward
+			i += 1
+			if d[i-1, 0] - d[i,0] == 1:
+				del_idx.append(i-1) # delete backward
+	else:
+		i += 1
+
 
 del_idx = np.unique(np.sort(np.array(del_idx)))
 # print(del_idx)
@@ -141,6 +155,19 @@ print("Post-cleanse Statistics")
 for level in range(num_levels):
 	print(f"Found {np.sum(d[:, 0] == level)} samples at level {level}.")
 
-# save dataset
-np.savetxt("MultilevelEstimators-multiQoIs.dat", d, delimiter=",", header="level, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9", fmt="%d, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e")
 
+# double-check again
+# for i in range(d.shape[0]):
+i = 0
+while i < d.shape[0]-1:
+	if d[i,0] == 0:
+		i += 1
+	elif d[i,0] - d[i+1,0] == 1:
+		i += 2
+	else:
+		print(f"Row {i+1} is not valid: {d[i]}")
+		i += 1
+
+# save dataset
+# np.savetxt("MultilevelEstimators-multiQoIs.dat", d, delimiter=",", header="level, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9", fmt="%d, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e, %.8e")
+np.savetxt("MultilevelEstimators-multiQoIs.dat", d, delimiter=",", header="level, q0, q1, q2, q3, q4, q5, q6, q7, q8, q9", fmt="%d, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f, %.8f")

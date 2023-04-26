aac547a58b352f16d986cc25a6870c127ddd1bd4
Anh Tran <anh.vt2@gmail.com>
2023-04-26 15:57:38 -0400

    add read_mlmc_results

---
+++ test_MLMC_multiQoIs_Mg/fakeWrapper/read_mlmc_results.py
@@ -0,0 +1,47 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import glob, os
+
+cost_per_level = [39,    365,    1955,    3305,    12487]
+num_levels = len(cost_per_level)
+
+# os.system('rm -v mlmc_cost')
+f = open('mlmc_cost.dat', 'w')
+f.write('# varepsilon, n, computational_cost, rmse\n')
+f.close()
+
+f = open('mlmc_frac_cost.txt', 'w')
+f.write('0	1	2	3	4	total\n')
+f.close()
+
+for fileName in glob.glob('log.mlmc.vareps-*'):
+	str_vareps = fileName.split('vareps')[1]
+	vareps = float(str_vareps)
+	print(f"Processing {fileName}")
+
+	f = open(fileName)
+	txt = f.readlines()
+	f.close()
+	num_lines = len(txt) # number of lines
+	n = np.zeros([num_levels])
+	try: n[0] = int(txt[-10].split('│')[6]) # number of samples used at level 0
+	except: n[0] = 0
+	try: n[1] = int(txt[-9].split('│')[6])  # number of samples used at level 1
+	except: n[1] = 0
+	try: n[2] = int(txt[-8].split('│')[6])  # number of samples used at level 2
+	except: n[2] = 0
+	try: n[3] = int(txt[-7].split('│')[6])  # number of samples used at level 3
+	except: n[3] = 0
+	try: n[4] = int(txt[-6].split('│')[6])  # number of samples used at level 4
+	except: n[4] = 0
+	rmse = float(txt[-5].split('≈')[1].replace('\n','')[:-1]) # rmse
+
+	f = open('mlmc_cost.dat', 'a+')
+	f.write('%.8e, %d, %d, %d, %d, %d, %.2f, %.8e\n' % (vareps, n[0], n[1], n[2], n[3], n[4], np.sum(n * cost_per_level), rmse))
+	f.close()
+
+	f = open('mlmc_frac_cost.txt', 'a+')
+	f.write('%d  %d  %d  %d  %d  %.8e\n' % (n[0], n[1], n[2], n[3], n[4], np.sum(n * cost_per_level)))
+	f.close()

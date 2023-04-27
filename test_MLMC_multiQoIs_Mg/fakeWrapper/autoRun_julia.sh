9d6f479063d8d8fa7d2f2bb3fa6d9393521fae43
Anh Tran <anh.vt2@gmail.com>
2023-04-26 22:06:05 -0400

    only run if log file not exists

--- test_MLMC_multiQoIs_Mg/fakeWrapper/autoRun_julia.sh
+++ test_MLMC_multiQoIs_Mg/fakeWrapper/autoRun_julia.sh
@@ -14,8 +14,14 @@ for i in $(seq $(cat vanilla_mc_cost.dat | wc -l)); do
 
 		sed -i "48s|.*|vareps = ${vareps}|" fakerun_multilevel_multiple_qoi.jl
 		python3 cleanseDataset.py # reset database
-		# rm -f nohup.out; nohup julia fakerun_multilevel_multiple_qoi.jl 2>&1 > log.mlmc.vareps-${vareps} &
-		julia fakerun_multilevel_multiple_qoi.jl 2>&1 > log.mlmc.vareps-${vareps}
+
+		if [ ! -e "log.mlmc.vareps-${vareps}" ]; then
+			# rm -f nohup.out; nohup julia fakerun_multilevel_multiple_qoi.jl 2>&1 > log.mlmc.vareps-${vareps} &
+			julia fakerun_multilevel_multiple_qoi.jl 2>&1 > log.mlmc.vareps-${vareps}
+		fi
+
+
+
 	fi
 done
 

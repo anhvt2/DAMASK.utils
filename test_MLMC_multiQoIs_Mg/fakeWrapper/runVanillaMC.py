827612f8a8a573c3b822d41d11f0b591621f49ce
Anh Tran <anh.vt2@gmail.com>
2023-04-27 11:35:32 -0400

    add MC for every level

--- test_MLMC_multiQoIs_Mg/fakeWrapper/runVanillaMC.py
+++ test_MLMC_multiQoIs_Mg/fakeWrapper/runVanillaMC.py
@@ -8,11 +8,11 @@ import socket
 
 os.system('python3 cleanseDataset.py')
 
-def computeNumberSamples(vareps):
+def computeNumberSamples(vareps, fixed_level=3):
 	d = np.loadtxt('MultilevelEstimators-multiQoIs.dat', skiprows=1, delimiter=',')
 	levels = d[:,0]
 	cost_per_level = [39,    365,    1955,    3305,    12487]
-	d_mc = d[np.where(levels == 3)]
+	d_mc = d[np.where(levels == fixed_level)] # default = 3
 	max_num_samples = d_mc.shape[0]
 	# calculate number of samples to converge
 	n = 3 # warm-up samples
@@ -28,14 +28,27 @@ def computeNumberSamples(vareps):
 		V = np.var(x, axis=0)
 		print(f"Take {n:<2d} samples. np.max(V) / n = {np.max(V) / n:<8.5f}. Tolerance: vareps**2 / 2 = {vareps**2 / 2:<13.8f}")
 		n += 1
-	return n, n * cost_per_level[-1], np.sqrt(2 * np.max(V) / n)
-
+	if n <= max_num_samples:
+		return n, n * cost_per_level[-1], np.sqrt(2 * np.max(V) / n)
+	else:
+		return np.nan, np.nan, np.nan
 
 os.system('rm -fv vanilla_mc_cost.dat')
 f = open('vanilla_mc_cost.dat', 'a+')
 f.write('# varepsilon, num_samples, computational_cost, rmse\n')
 for vareps in np.arange(1.0, 0.02, -0.02):
-	n, comp_cost, rmse = computeNumberSamples(vareps)
+	n, comp_cost, rmse = computeNumberSamples(vareps, fixed_level=3)
 	f.write('%.8e, %d, %.2f, %.8e\n' % (vareps, n, comp_cost, rmse))
 
 f.close()
+
+# also run at each level
+for level in range(5):
+	f = open('vanilla_mc_cost_level-%d.dat' % level, 'w')
+	f.write('# varepsilon, num_samples, computational_cost, rmse\n')
+	for vareps in np.arange(1.0, 0.02, -0.02):
+		n, comp_cost, rmse = computeNumberSamples(vareps, fixed_level=level)
+		f.write('%.8e, %.1f, %.2f, %.8e\n' % (vareps, n, comp_cost, rmse))
+
+	f.close()
+

20ecc80c3cd26571d80b43552637a36bed27f699
Anh Tran <anh.vt2@gmail.com>
2023-05-11 22:53:18 -0400

    do not show edge in pyvista for 2d RVE

--- plotms2d.py
+++ plotms2d.py
@@ -40,16 +40,20 @@ for filename in glob.glob('*.vtr'): # screenshot for all *.vtr files
 
 	# pl = pyvista.Plotter()
 	pl = pyvista.Plotter(off_screen=True)
-	pl.add_mesh(msMesh, show_edges=True, line_width=1, cmap=cmap)
+	pl.add_mesh(msMesh, show_edges=False, line_width=1, cmap=cmap)
 	pl.background_color = "white"
+	# light = pyvista.Light(position=(16, 16, 10), color='white')
+	# light.positional = True
 	pl.view_xy()
 	pl.remove_scalar_bar()
+	# pl.add_light(light)
 	# pl.show(screenshot='%s.png' % filename.split('.')[0])
 	# pl.show()
-	if nameTag == '':
-		pl.screenshot(filename.split('.')[0] + '.png', window_size=[1860,968])
-	else:
-		pl.screenshot(filename.split('.')[0] + '_' + nameTag + '.png', window_size=[1860,968])
 	# pl.close()
+	if nameTag == '':
+		pl.screenshot(filename.split('.')[0] + '.png', window_size=[1860*3,968*3])
+		# pl.screenshot(filename.split('.')[0] + '.png', window_size=[3200,3200])
+	else:
+		pl.screenshot(filename.split('.')[0] + '_' + nameTag + '.png', window_size=[968*3,968*3])
 
 

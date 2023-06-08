8ee73d69f251b0d3da27d8ff17c4665cb904d1c2
Anh Tran <anh.vt2@gmail.com>
2023-06-08 14:50:27 -0400

    automate postProc.sh

--- test_tensile_dogbone-spk2damask/damask/postProc.sh
+++ test_tensile_dogbone-spk2damask/damask/postProc.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
-rm -f nohup.out; nohup postResults --cr fp,f,p,grainrotation,texture --split --separation x,y,z --increments --range 1 44 1 spk_dump_12_out_tension.spectralOut 2>&1 > log.postResults &
+geomFileName="spk_dump_12_out" # ${geomFileName}.geom
+loadFileName="tension" # ${loadFileName}.load
+
+rm -f nohup.out; nohup postResults --cr fp,f,p,grainrotation,texture --split --separation x,y,z --increments --range 1 44 1 ${geomFileName}_${loadFileName}.spectralOut 2>&1 > log.postResults &
 
 cd postProc
-addStrainTensors -0 -v <fileName>.txt
-addCauchy <fileName>.txt
-addMises -s Cauchy <fileName>.txt
-addStrainTensors --left --logarithmic <fileName>.txt
-addMises -e 'ln(V)' <fileName>.txt
-addDisplacement --nodal <fileName>.txt
+for fileName in $(ls -1v ${geomFileName}_${loadFileName}*.txt); do
+	addStrainTensors -0 -v ${fileName}.txt
+	addCauchy ${fileName}.txt
+	addMises -s Cauchy ${fileName}.txt
+	addStrainTensors --left --logarithmic ${fileName}.txt
+	addMises -e 'ln(V)' ${fileName}.txt
+	addDisplacement --nodal ${fileName}.txt
+	echo "done processing ${fileName}."
+done

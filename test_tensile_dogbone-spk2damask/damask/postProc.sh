975d3ff21ba65700e35c85cb30b07d28fc500647
Anh Tran <anh.vt2@gmail.com>
2023-06-12 07:06:48 -0600

    update post-processing pipeline

--- test_tensile_dogbone-spk2damask/damask/postProc.sh
+++ test_tensile_dogbone-spk2damask/damask/postProc.sh
@@ -5,18 +5,18 @@
 geomFileName="spk_dump_12_out" # ${geomFileName}.geom
 loadFileName="tension" # ${loadFileName}.load
 
-# rm -f nohup.out; nohup postResults --cr fp,f,p,grainrotation,texture --split --separation x,y,z --increments --range 1 44 1 ${geomFileName}_${loadFileName}.spectralOut 2>&1 > log.postResults &
+# how to run in quiet mode: rm -f nohup.out; nohup postResults --cr fp,f,p,grainrotation,texture --split --separation x,y,z --increments --range 1 44 1 ${geomFileName}_${loadFileName}.spectralOut 2>&1 > log.postResults &
 
 cd postProc
 for fileName in $(ls -1v ${geomFileName}_${loadFileName}*.txt); do
 	fileName=$(echo ${fileName} | cut -d. -f1)
 
-#	addStrainTensors -0 -v ${fileName}.txt
-#	addCauchy ${fileName}.txt
-#	addMises -s Cauchy ${fileName}.txt
-#	addStrainTensors --left --logarithmic ${fileName}.txt
-#	addMises -e 'ln(V)' ${fileName}.txt
-#	addDisplacement --nodal ${fileName}.txt
+	addStrainTensors -0 -v ${fileName}.txt
+	addCauchy ${fileName}.txt
+	addMises -s Cauchy ${fileName}.txt
+	addStrainTensors --left --logarithmic ${fileName}.txt
+	addMises -e 'ln(V)' ${fileName}.txt
+	addDisplacement --nodal ${fileName}.txt
 
 	vtk_rectilinearGrid ${fileName}.txt
 

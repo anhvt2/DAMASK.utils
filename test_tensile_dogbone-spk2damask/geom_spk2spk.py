c5d0c7d7690a1b409a67ace0897e34ad58a301b1
Anh Tran <anh.vt2@gmail.com>
2023-07-03 09:57:19 -0400

    autopep8 reformat

--- test_tensile_dogbone-spk2damask/geom_spk2spk.py
+++ test_tensile_dogbone-spk2damask/geom_spk2spk.py
@@ -23,7 +23,10 @@
 """
 
 import numpy as np
-import os, sys, time, glob
+import os
+import sys
+import time
+import glob
 import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
@@ -33,17 +36,18 @@ from natsort import natsorted, ns
 
 parser = argparse.ArgumentParser()
 parser.add_argument("-vti", "--vti",     type=str, required=True)
-parser.add_argument("-p"  , "--phase",   type=str, required=True)
+parser.add_argument("-p", "--phase",   type=str, required=True)
 # parser.add_argument("-p"  , "--void_id", type=str, False=True, default=np.inf)
 args = parser.parse_args()
 
-vtiFileNames  = args.vti # 'potts_3d.*.vti'
-phaseFileName = args.phase # 'phase_dump_12_out.npy'
+vtiFileNames = args.vti  # 'potts_3d.*.vti'
+phaseFileName = args.phase  # 'phase_dump_12_out.npy'
 
 try:
     phase = np.load(phaseFileName)
 except OSError:
     print(f"File {phaseFileName} does not exist.")
+
 
 def getDumpMs(dumpFileName):
     """
@@ -52,34 +56,39 @@ def getDumpMs(dumpFileName):
     dumpFile = open(dumpFileName)
     dumptxt = dumpFile.readlines()
     dumpFile.close()
-    for i in range(20): # look for header info in first 20 lines
+    for i in range(20):  # look for header info in first 20 lines
         tmp = dumptxt[i]
         if 'BOX BOUNDS' in tmp:
-            Nx = int(dumptxt[i+1].replace('\n','').replace('0 ', '').replace(' ', ''))
-            Ny = int(dumptxt[i+2].replace('\n','').replace('0 ', '').replace(' ', ''))
-            Nz = int(dumptxt[i+3].replace('\n','').replace('0 ', '').replace(' ', ''))
+            Nx = int(dumptxt[i+1].replace('\n',
+                     '').replace('0 ', '').replace(' ', ''))
+            Ny = int(dumptxt[i+2].replace('\n',
+                     '').replace('0 ', '').replace(' ', ''))
+            Nz = int(dumptxt[i+3].replace('\n',
+                     '').replace('0 ', '').replace(' ', ''))
             break
-    header = np.array(dumptxt[i+4].replace('\n','').replace('ITEM: ATOMS ', '').split(' '), dtype=str)
+    header = np.array(
+        dumptxt[i+4].replace('\n', '').replace('ITEM: ATOMS ', '').split(' '), dtype=str)
     d = np.loadtxt(dumpFileName, skiprows=9, dtype=int)
-    num_grains = len(np.unique(d[:,1]))
-    old_grain_ids = np.unique(d[:,1])
-    new_grain_ids = range(len(np.unique(d[:,1])))
-    m = np.zeros([Nx, Ny, Nz]) # initialize
+    num_grains = len(np.unique(d[:, 1]))
+    old_grain_ids = np.unique(d[:, 1])
+    new_grain_ids = range(len(np.unique(d[:, 1])))
+    m = np.zeros([Nx, Ny, Nz])  # initialize
     for ii in range(len(d)):
-        i = int(d[ii,np.where(header=='x')[0][0]]) # 'x'
-        j = int(d[ii,np.where(header=='y')[0][0]]) # 'y'
-        k = int(d[ii,np.where(header=='z')[0][0]]) # 'z'
-        grain_id = int(d[ii,1]) # or d[i,2] -- both are the same
+        i = int(d[ii, np.where(header == 'x')[0][0]])  # 'x'
+        j = int(d[ii, np.where(header == 'y')[0][0]])  # 'y'
+        k = int(d[ii, np.where(header == 'z')[0][0]])  # 'z'
+        grain_id = int(d[ii, 1])  # or d[i,2] -- both are the same
         # option: DO re-enumerating
         lookup_idx = np.where(old_grain_ids == grain_id)[0][0]
         new_grain_id = new_grain_ids[lookup_idx]
-        m[i,j,k] = new_grain_id
+        m[i, j, k] = new_grain_id
         # option: DO NOT re-enumerating
         # m[i,j,k] = grain_id # TODO: implement re-enumerate grain_id
         # print(f"finish ({x},{y}, {z})")
     return m, Nx, Ny, Nz, num_grains
 
-t_start = time.time() # tic
+
+t_start = time.time()  # tic
 
 for vtiFileName in natsorted(glob.glob(vtiFileNames)):
     outFileName = 'masked_' + vtiFileName
@@ -91,12 +100,13 @@ for vtiFileName in natsorted(glob.glob(vtiFileNames)):
     for i in range(Nx):
         for j in range(Ny):
             for k in range(Nz):
-                if phase[i,j,k] == np.inf:
-                    spin[i,j,k] = 1 # assign void
+                if phase[i, j, k] == np.inf:
+                    spin[i, j, k] = 1  # assign void
     vtiData['Spin'] = spin.T.flatten()
     vtiData.save(outFileName)
     print(f"Finished processing {vtiFileName}!\n")
 
 
-elapsed = time.time() - t_start # toc
-print("geom_spk2spk.py: finished in {:5.2f} seconds.\n".format(elapsed), end="")
+elapsed = time.time() - t_start  # toc
+print("geom_spk2spk.py: finished in {:5.2f} seconds.\n".format(
+    elapsed), end="")

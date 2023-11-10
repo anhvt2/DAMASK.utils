2da2f61ccbaa6a7edf5a738d5ca7199c2ac9c88d
Anh Tran <anh.vt2@gmail.com>
2023-11-10 11:36:26 -0500

    update findGaugeLocations.py for padded air microstructure
    * more robust and center probe (remove random probe)

--- test_singleCrystal_ROM/findGaugeLocations.py
+++ test_singleCrystal_ROM/findGaugeLocations.py
@@ -4,14 +4,24 @@
 '''
 
 import numpy as np
-import glob
+import glob, os
+import argparse
 
-fileName = glob.glob('*.geom')[0]
-ms = np.load(fileName.split('.')[0] + '.npy')
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--geom", type=str, required=True)
+args = parser.parse_args()
+
+fileName = args.geom
+msFileName = fileName.split('.')[0] + '.npy'
+if os.path.exists(msFileName):
+    print(f"\nCheckpoint: ms {msFileName} .npy exists. Proceed forward.")
+else:
+    raise Exception(f"\n{msFileName} does not exist. Please run geom2npy.py to convert .geom to .npy array.") 
+ms = np.load(msFileName)
 Nx, Ny, Nz = ms.shape
 
-# Take a random 2d slice of ms
-slice2d = ms[:,0,:] - 1 # y-direction is extrusion direction - not important, so any would work
+# Take centered 2d slice of ms
+slice2d = ms[:, int(np.floor(Ny/2)),:] - 1
 
 # Assume void id is '1'
 
@@ -32,9 +42,22 @@ for j in range(Nz):
     slice2dx = np.sum(slice2d,axis=1)
     xLocs = np.where(slice2dx == np.max(slice2dx))
 
+'''
+Find number of air voxels and bound its regions by tracing a ray at center
+'''
+segment = ms[int(np.floor(Nx/2)),:,int(np.floor(Nz/2))] - 1
+yLocs = np.where(segment == np.max(segment))
+
+# Diagnostics
 print(f'{np.min(xLocs)} <= x <= {np.max(xLocs)}')
+print(f'{np.min(yLocs)} <= y <= {np.max(yLocs)}')
 print(f'{np.min(zLocs)} <= z <= {np.max(zLocs)}')
 
+# Write gaugeFilter.txt
+
 f = open('gaugeFilter.txt', 'w')
-f.write('%d <= x <= %d and %d <= z <= %d\n' % (np.min(xLocs), np.max(xLocs), np.min(zLocs), np.max(zLocs)))
+f.write('%d <= x <= %d and %d <= y <= %d and %d <= z <= %d\n' % 
+    (   np.min(xLocs), np.max(xLocs), 
+        np.min(yLocs), np.max(yLocs), 
+        np.min(zLocs), np.max(zLocs)))
 f.close()

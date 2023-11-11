9dbfb14be0816bb78f4d1ddc6db3be6d4eb5048e
Anh Tran <anh.vt2@gmail.com>
2023-11-11 17:46:27 -0500

    correct geomFileName and loadFileName for postproc

--- test_CBayes_tensileDogbone/test.sh
+++ test_CBayes_tensileDogbone/test.sh
@@ -1,26 +1,25 @@
 #!/bin/bash
-# postResults \
-# --cr fp,f,p \
-# --split --separation x,y,z \
-# --increments \
-# --range 10 10 1 singleCrystal_res_50um_tension.spectralOut
+python3 findGaugeLocations.py # dump gaugeFilter.txt
+postResults *.spectralOut --cr f,p --filter $(cat gaugeFilter.txt)
+cd postProc/
+addStrainTensors ${geomFileName}_${loadFileName}.txt --left --logarithmic
+addCauchy ${geomFileName}_${loadFileName}.txt
+addMises ${geomFileName}_${loadFileName}.txt --strain 'ln(V)' --stress Cauchy
+filterTable < ${geomFileName}_${loadFileName}.txt --white inc,'Mises(ln(V))','Mises(Cauchy)' > stress_strain.log
+cd ..
+
+postResults \
+--cr fp,f,p \
+--split --separation x,y,z \
+--increments \
+--range 1 10 1 ${geomFileName}_${loadFileName}.spectralOut
 
 cd postProc
-# for i in $(seq 10); do
-#    addStrainTensors -0 -v singleCrystal_res_50um_tension_inc${i}.txt
-#    addCauchy singleCrystal_res_50um_tension_inc${i}.txt
-#    addMises -s Cauchy singleCrystal_res_50um_tension_inc${i}.txt
-#    addStrainTensors --left --logarithmic singleCrystal_res_50um_tension_inc${i}.txt
-#    addMises -e 'ln(V)' singleCrystal_res_50um_tension_inc${i}.txt
-#    addDisplacement --nodal singleCrystal_res_50um_tension_inc${i}.txt
-# done
-
-for fileName in $(ls singleCrystal_res_50um_tension_inc*.txt); do
-    addStrainTensors -0 -v ${fileName}
-    addCauchy ${fileName}
-    addMises -s Cauchy ${fileName}
-    addStrainTensors --left --logarithmic ${fileName}
-    addMises -e 'ln(V)' ${fileName}
-    addDisplacement --nodal ${fileName}
+for i in $(seq 10); do
+    addStrainTensors -0 -v ${geomFileName}_${loadFileName}_inc${i}.txt
+    addCauchy ${geomFileName}_${loadFileName}_inc${i}.txt
+    addMises -s Cauchy ${geomFileName}_${loadFileName}_inc${i}.txt
+    addStrainTensors --left --logarithmic ${geomFileName}_${loadFileName}_inc${i}.txt
+    addMises -e 'ln(V)' ${geomFileName}_${loadFileName}_inc${i}.txt
+    addDisplacement --nodal ${geomFileName}_${loadFileName}_inc${i}.txt
 done
-

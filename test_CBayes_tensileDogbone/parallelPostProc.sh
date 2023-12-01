a8d331c1a5737f0b7390c18bf0cb34db1731fee9
Anh Tran <anh.vt2@gmail.com>
2023-12-01 14:15:31 -0500

    add parallel post-processing sbatch

--- test_CBayes_tensileDogbone/parallelPostProc.sh
+++ test_CBayes_tensileDogbone/parallelPostProc.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
-geomFileName="spk_dump_20_out" # ${geomFileName}.geom
-loadFileName="tension" # ${loadFileName}.load
+geomFileName='padded_voidSeeded_1.500pc_spk_dump_12_out'
+loadFileName='tension'
 
 cd postProc/
 for fileName in $(ls -1v ${geomFileName}_${loadFileName}*.txt); do
     fileName=$(echo ${fileName} | cut -d. -f1)
     cp ../templatePostProc-1File.sh postProc-${fileName}.sh
+    cp ../sbatch.postprocDamask.srn postProc-${fileName}.sh
     sed -i "4s|.*|fileName=\"${fileName}\"|" postProc-${fileName}.sh
+    sed -i "17s|.*|fileName=\"${fileName}\"|" sbatch.postprocDamask-${fileName}.srn
     # rm -f nohup.out; nohup bash ${fileName}.sh 2>&1 > ${fileName}.sh.log & # execute postProc in background
     echo ${fileName}
 done

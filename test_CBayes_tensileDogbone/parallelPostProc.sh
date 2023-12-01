035af8eb09050276a0cd6523ec63f5a6d74723bc
Anh Tran <anh.vt2@gmail.com>
2023-12-01 12:25:28 -0700

    debug: fix fileName

--- test_CBayes_tensileDogbone/parallelPostProc.sh
+++ test_CBayes_tensileDogbone/parallelPostProc.sh
@@ -8,7 +8,7 @@ for fileName in $(ls -1v ${geomFileName}_${loadFileName}*.txt); do
     fileName=$(echo ${fileName} | rev | cut -c 5- | rev)
     # fileName=$(echo ${fileName} | cut -d. -f1) # deprecated
     cp ../templatePostProc-1File.sh postProc-${fileName}.sh
-    cp ../sbatch.postprocDamask.srn postProc-${fileName}.sh
+    cp ../sbatch.postprocDamask.srn sbatch.postprocDamask-${fileName}.srn
     sed -i "4s|.*|fileName=\"${fileName}\"|" postProc-${fileName}.sh
     sed -i "17s|.*|fileName=\"${fileName}\"|" sbatch.postprocDamask-${fileName}.srn
     # rm -f nohup.out; nohup bash ${fileName}.sh 2>&1 > ${fileName}.sh.log & # execute postProc in background

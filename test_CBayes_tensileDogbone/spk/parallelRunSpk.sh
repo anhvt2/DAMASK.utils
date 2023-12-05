be8ad5fe37131e0c52c3004e1333ed32ca781d89
Anh Tran <anh.vt2@gmail.com>
2023-12-04 19:18:03 -0700

    debug parallelRunSpk: fix folder name

--- test_CBayes_tensileDogbone/spk/parallelRunSpk.sh
+++ test_CBayes_tensileDogbone/spk/parallelRunSpk.sh
@@ -2,7 +2,7 @@
 
 # for i in $(seq -f "%03g" 1 100); do
 for i in $(seq 1 100); do
-	folderName="res-10um-run-${i}"
+	folderName="res-50um-run-${i}"
 	cp -rfv res-50um/ $folderName
 	cd $folderName
 	ssubmit

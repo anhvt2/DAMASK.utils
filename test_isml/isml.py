a881dce8fdd2285ef22aa524b036bdce475ec88a
Anh Tran <anh.vt2@gmail.com>
2024-02-19 22:30:58 -0500

    update panda frame

--- test_isml/isml.py
+++ test_isml/isml.py
@@ -21,42 +21,50 @@ df = pd.DataFrame(data, columns=headers)
 
 # https://scikit-learn.org/0.20/auto_examples/plot_anomaly_comparison.html
 
-n_samples = 300
-outliers_fraction = 0.15
+n_samples = df.shape[0]
+outliers_fraction = 0.02 # percentage of void: soft parameter (to be studied)
 n_outliers = int(outliers_fraction * n_samples)
 n_inliers = n_samples - n_outliers
 
 # define outlier/anomaly detection methods to be compared
 anomaly_algorithms = [
-    ("Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
-    ("One-Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel="rbf",
-                                      gamma=0.1)),
-    ("Isolation Forest", IsolationForest(behaviour='new',
-                                         contamination=outliers_fraction,
-                                         random_state=42)),
-    ("Local Outlier Factor", LocalOutlierFactor(
-        n_neighbors=35, contamination=outliers_fraction))]
+	("Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
+	("One-Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel="rbf",
+									  gamma=0.1)),
+	("Isolation Forest", IsolationForest(contamination=outliers_fraction,
+										 random_state=42)),
+	("Local Outlier Factor", LocalOutlierFactor(
+		n_neighbors=35, contamination=outliers_fraction))]
+
+resolution = 50
+for selStr in ['1_pos', '2_pos', '3_pos']:
+	df[selStr] -= (resolution/2.)
+	df[selStr] /= (resolution)
+
+# df.keys()
+stress = df['Mises(Cauchy)']
+strain = df['Mises(Cauchy)']
 
 # Add outliers
 X = np.concatenate([X, rng.uniform(low=-6, high=6,
-                   size=(n_outliers, 2))], axis=0)
+				   size=(n_outliers, 2))], axis=0)
 
 for name, algorithm in anomaly_algorithms:
-    t0 = time.time()
-    algorithm.fit(X)
-    t1 = time.time()
-    plt.subplot(len(datasets), len(anomaly_algorithms), plot_num)
-    if i_dataset == 0:
-        plt.title(name, size=18)
+	t0 = time.time()
+	algorithm.fit(X)
+	t1 = time.time()
+	plt.subplot(len(datasets), len(anomaly_algorithms), plot_num)
+	if i_dataset == 0:
+		plt.title(name, size=18)
 
-    # fit the data and tag outliers
-    if name == "Local Outlier Factor":
-        y_pred = algorithm.fit_predict(X)
-    else:
-        y_pred = algorithm.fit(X).predict(X)
+	# fit the data and tag outliers
+	if name == "Local Outlier Factor":
+		y_pred = algorithm.fit_predict(X)
+	else:
+		y_pred = algorithm.fit(X).predict(X)
 
-    # plot the levels lines and the points
-    if name != "Local Outlier Factor":  # LOF does not implement predict
-        Z = algorithm.predict(np.c_[xx.ravel(), yy.ravel()])
-        Z = Z.reshape(xx.shape)
-        plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='black')
+	# plot the levels lines and the points
+	if name != "Local Outlier Factor":  # LOF does not implement predict
+		Z = algorithm.predict(np.c_[xx.ravel(), yy.ravel()])
+		Z = Z.reshape(xx.shape)
+		plt.contour(xx, yy, Z, levels=[0], linewidths=2, colors='black')

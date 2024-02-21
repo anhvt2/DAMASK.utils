f7ff6ec69fee6575d1056888b43d3b1b6872e3c7
Anh Tran <anh.vt2@gmail.com>
2024-02-20 21:38:16 -0500

    reverse order: read ground truth first then predict later

--- test_isml/isml.py
+++ test_isml/isml.py
@@ -14,27 +14,11 @@ fileHandler = open(fileName)
 txt = fileHandler.readlines()
 fileHandler.close()
 
+### Pre-process
 numHeaderRows = int(txt[0].split('\t')[0])
 headers = txt[numHeaderRows].replace('\n', '').split('\t')
 data = np.loadtxt(fileName, skiprows=numHeaderRows+1)
 df = pd.DataFrame(data, columns=headers)
-
-# Define outlier/anomaly detection methods to be compared
-# https://scikit-learn.org/0.20/auto_examples/plot_anomaly_comparison.html
-n_samples = df.shape[0]
-outliers_fraction = 0.02 # percentage of void: soft parameter (to be studied)
-n_outliers = int(outliers_fraction * n_samples)
-n_inliers = n_samples - n_outliers
-
-anomaly_algorithms = [
-	(0, "Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
-	(1, "One-Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel="rbf",
-									  gamma=0.1)),
-	(2, "Isolation Forest", IsolationForest(contamination=outliers_fraction,
-										 random_state=42)),
-	(3, "Local Outlier Factor", LocalOutlierFactor(
-		n_neighbors=35, contamination=outliers_fraction))]
-
 resolution = 50
 for selStr in ['1_pos', '2_pos', '3_pos']:
 	df[selStr] -= (resolution/2.)
@@ -47,37 +31,52 @@ X = df[['Mises(Cauchy)', 'Mises(ln(V))']]
 X = np.atleast_2d(X)
 # X = (X - X.min()) / (X.max() - X.min()) * 2 - 1
 from sklearn.preprocessing import MinMaxScaler
-X = MinMaxScaler(feature_range=(-1, 1), copy=True, clip=False).fit_transform(X)
+X = MinMaxScaler(feature_range=(-1, 1), copy=True, clip=False).fit_transform(X) # rescale to [-1,1]
 
-# # Anomaly detection
-# y_pred = np.zeros([X.shape[0], len(anomaly_algorithms)])
-# for i, name, algorithm in anomaly_algorithms:
-# 	t0 = time.time()
-# 	algorithm.fit(X)
-# 	t1 = time.time()
-# 	# fit the data and tag outliers
-# 	if name == "Local Outlier Factor":
-# 		y_pred[:,i] = algorithm.fit_predict(X)
-# 	else:
-# 		y_pred[:,i] = algorithm.fit(X).predict(X)
-
-# y_pred = y_pred * (-1) / 2. + 0.5
-# np.save('y_pred.npy', y_pred)
-y_pred = np.load('y_pred.npy')
-
-# # Read ground true - y3d: void true/false in 3d .npy; y: flatten 1d array
+### Read ground true & extract info from seedVoid.log - y3d: void true/false in 3d .npy; y: flatten 1d array
 y3d = np.load('padded_voidSeeded_2.000pc_spk_dump_12_out.npy')
-# extract info from seedVoid.log
 y3d = (y3d>=2) & (y3d<= 4806) # extract void
-numTotalVoxels = 120 * 20 * 200
+y3d = y3d.astype(int)
+numTotalVoxels = 120 * 24 * 200 # adjust from seedVoid.log due to padding air to break pbc
 numSolidVoxels = 240260
-numVoidVoxels = 4805
-# y3d = y3d.astype(int)
-# y = np.zeros(numTotalVoxels)
-# for i in range(numTotalVoxels): # flatten 3d array
-#     y[i] = y3d[ int(df['1_pos'][i]), int(df['2_pos'][i]), int(df['3_pos'][i]) ]
-# np.save('y.npy', y)
-y = np.load('y.npy')
+numVoidVoxels = y3d.sum() # 4805
+y = np.zeros(numTotalVoxels)
+for i in range(numTotalVoxels): # flatten 3d array
+	y[i] = y3d[ int(df['1_pos'][i]), int(df['2_pos'][i]), int(df['3_pos'][i]) ]
 
-# Plot
+np.save('y.npy', y)
+# y = np.load('y.npy') # quick n' dirty bypass
+
+### Anomaly detection
+# https://scikit-learn.org/0.20/auto_examples/plot_anomaly_comparison.html
+n_samples = df.shape[0]
+outliers_fraction = y3d.sum() / X.shape[0] # 0.02 # percentage of void: soft parameter (to be studied)
+n_outliers = int(outliers_fraction * n_samples)
+n_inliers = n_samples - n_outliers
+
+anomaly_algorithms = [
+	(0, "Robust covariance", EllipticEnvelope(contamination=outliers_fraction)),
+	(1, "One-Class SVM", svm.OneClassSVM(nu=outliers_fraction, kernel="rbf",
+									  gamma=0.1)),
+	(2, "Isolation Forest", IsolationForest(contamination=outliers_fraction,
+										 random_state=42)),
+	(3, "Local Outlier Factor", LocalOutlierFactor(
+		n_neighbors=35, contamination=outliers_fraction))]
+
+y_pred = np.zeros([X.shape[0], len(anomaly_algorithms)])
+for i, name, algorithm in anomaly_algorithms:
+	t0 = time.time()
+	algorithm.fit(X)
+	t1 = time.time()
+	# fit the data and tag outliers
+	if name == "Local Outlier Factor":
+		y_pred[:,i] = algorithm.fit_predict(X)
+	else:
+		y_pred[:,i] = algorithm.fit(X).predict(X)
+
+y_pred = y_pred * (-1) / 2. + 0.5
+np.save('y_pred.npy', y_pred)
+# y_pred = np.load('y_pred.npy') # quick n' dirty bypass
+
+### Plot
 

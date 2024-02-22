c64422cfb6f5f03f64e69b80490d88c599ca1759
Anh Tran <anh.vt2@gmail.com>
2024-02-22 09:37:33 -0500

    update plotting pipeline

--- test_isml/test.sh
+++ test_isml/test.sh
@@ -3,5 +3,6 @@
 for fileName in 'RobustCovariance' 'OneClassSVM' 'IsolationForest' 'LOF'; do
     python3 ../npy2geom.py --npy ${fileName}.npy
     geom_check ${fileName}.geom
+    python3 plotVoid3dAnomalyDetection.py --fileName ${fileName}.vtr
 done
 

4f4a7d143cbe9ff955ba3bdde89c6f7fa79e7446
Anh Tran <anh.vt2@gmail.com>
2024-04-24 09:08:12 -0600

    add more files to delete

--- test_tensile_dogbone-spk2damask/spk/cleanup.sh
+++ test_tensile_dogbone-spk2damask/spk/cleanup.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 rm -f *vti
 rm -f dump*.out
-rm -f cluster.dat log.spparks vti.tar.gz log.pid
+rm -f cluster.dat log.spparks vti.tar.gz log.pid test.tar.gz
+rm -f *.jpg
+rm -f dump.additive_dogbone.*

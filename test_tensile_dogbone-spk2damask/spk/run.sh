6136b8e3b6401425e3507cfea075d7e718d87194
Anh Tran <anh.vt2@gmail.com>
2024-04-26 11:00:19 -0400

    adapt number of processors nProc depending on local hpc

--- test_tensile_dogbone-spk2damask/spk/run.sh
+++ test_tensile_dogbone-spk2damask/spk/run.sh
@@ -3,4 +3,4 @@ rm -f *jpg *vti dump*
 ln -sf ../in.potts_additive_dogbone .
 fakeNProc=$(nproc --all)
 nProc=$((${fakeNProc}/2))
-mpirun -np 8 spk < in.potts_additive_dogbone
+mpirun -np ${nProc} spk < in.potts_additive_dogbone

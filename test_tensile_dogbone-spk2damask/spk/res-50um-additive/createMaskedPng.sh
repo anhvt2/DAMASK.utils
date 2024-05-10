6997c35ce081106a4ec676dbd55cb3228e5a8b89
Anh Tran <anh.vt2@gmail.com>
2024-05-10 12:28:18 -0400

    add createMasked{Png,Vti}.sh and link to geom_spk2spk.py

---
+++ test_tensile_dogbone-spk2damask/spk/res-50um-additive/createMaskedPng.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+rm masked_potts_3d.*.png
+
+for i in $(seq 0 2807); do
+# for i in 0 1 2 3 4 5 6 7 8 9 10 15 20 25 30 40 50 60 80 100 150 200 250 300 450 400 550 500; do
+    python3 plotms3d_maskedDogbone.py --fileName="masked_potts_3d.${i}.vti"
+done

0248d0384a07e1876f10ef6e3f32fc1431a3988a
Anh Tran <anh.vt2@gmail.com>
2024-05-10 12:40:14 -0400

    debug: update file name

--- test_tensile_dogbone-spk2damask/spk/res-50um-additive/createMaskedVti.sh
+++ test_tensile_dogbone-spk2damask/spk/res-50um-additive/createMaskedVti.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 for i in $(seq 0 2807); do
-    python3 geom_spk2spk.py --vti="potts_3d.${i}.vti" --phase="phase_dump_12_out.npy"
+    python3 geom_spk2spk.py --vti="additive_dogbone.${i}.vti" --phase="phase_dump_12_out.npy"
 done

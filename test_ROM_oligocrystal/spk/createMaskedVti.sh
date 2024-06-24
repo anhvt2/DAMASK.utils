ad7f1281429f4e241380c16a02a312fdf1c7e3e3
Anh Tran <anh.vt2@gmail.com>
2024-06-24 09:23:43 -0400

    update utils script

--- test_ROM_oligocrystal/spk/createMaskedVti.sh
+++ test_ROM_oligocrystal/spk/createMaskedVti.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 
-for i in $(seq 0 516); do
-    python3 geom_spk2spk.py --vti="potts_3d.${i}.vti" --phase="phase_dump_12_out.npy"
+# for i in $(seq 1000 100 1500); do
+# for i in $(seq $(ls *vti | wc -l)); do
+#     python3 geom_spk2spk.py --vti="potts_3d.${i}.vti" --phase="phase_dump_12_out.npy"
+# done
+
+for vtiFile in $(ls potts_3d*vti); do
+    python3 geom_spk2spk.py --vti="${vtiFile}" --phase="phase_dump_12_out.npy"
 done

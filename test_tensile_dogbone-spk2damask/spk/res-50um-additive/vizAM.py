9ec73cd20b5a805506578a8c459ecdae8e32462d
Anh Tran <anh.vt2@gmail.com>
2024-06-29 22:24:00 -0400

    test functional vizAM.py

--- test_tensile_dogbone-spk2damask/spk/res-50um-additive/vizAM.py
+++ test_tensile_dogbone-spk2damask/spk/res-50um-additive/vizAM.py
@@ -1,24 +1,34 @@
 
 """
+Example how to use:
+python3 vizAM.py --npyFolderName='npy' --phaseFileName='phase_dump_12_out.npy'
+
+then
+python3 npy2png.py --threshold=1
+
 This script 
-	(1) converts a series of microstructures formatted in *.npy (must be)
-	(2) along with a chosen phase (for example, dogbone)
-	(3) to a series of images (that could be converted to video) for illustration purpose
+    (1) converts a series of microstructures formatted in *.npy (must be)
+    (2) along with a chosen phase (for example, dogbone)
+    (3) to a series of images (that could be converted to video) for illustration purpose
 
 * Work for any voxelized-STL file.
 
 How?
+Step-1: mask according to phase
+Step-2: only show difference compared to initial microstructure
+
+Other random thoughts:
 - Convert *.vti to *.npy
 - Dump masked *.npy based on (1) phase and (2) difference with the initial condition *.npy
 - Convert masked *.npy to *.geom
 - Convert *.geom to *.vtr
 OR
-- Convert masked *.npy directly to *.vtr # https://tutorial.pyvista.org/tutorial/02_mesh/solutions/c_create-uniform-grid.html
-
+- ~~Convert masked *.npy directly to *.vtr # https://tutorial.pyvista.org/tutorial/02_mesh/solutions/c_create-uniform-grid.html~~ (done, see `npy2png.py`)
 
 Show us with threshold, hide the rest.
 - Always show the final us (with opacity=0 for consistent grain ID colormap)
 - Only show the diff b/w the current us and the initial us, but NOT include masked phase
+
 """
 
 import numpy as np
@@ -36,76 +46,40 @@ args = parser.parse_args()
 npyFolderName = args.npyFolderName # 'npy'
 phaseFileName = args.phaseFileName # 'phase_dump_12_out.npy'
 
+def maskMs(phase, ms):
+    '''
+    This function masks a microstructure (ms) using phase.
+    The resulting microstructure has grainID += 1 due to masking.
+    '''
+    maskedPhase = np.array(~np.isinf(phase), dtype=int)
+    maskedMs = np.multiply(maskedPhase, ms)
+    maskedMs += 1
+    return maskedMs
+
+def highlightMs(currentMs, initialMs):
+    '''
+    This function 
+        (1) compares a current ms with the initial ms,
+        (2) and masks every element as 1 if the same, retain the same if different
+    '''
+    dimensions = currentMs.shape
+    mask = np.array(currentMs != initialMs, dtype=int)
+    highlightedCurrentMs = np.multiply(currentMs, mask)
+    return highlightedCurrentMs
+
+
 npyFolderList = natsorted(glob.glob(npyFolderName + '/*.npy'))
 phase = np.load(phaseFileName)
 initialMs = maskMs(phase, np.load(npyFolderList[0]))
 lastMs    = maskMs(phase, np.load(npyFolderList[-1]))
 
-def maskMs(phase, ms):
-	'''
-	This function masks a microstructure (ms) using phase.
-	The resulting microstructure has grainID += 1 due to masking.
-	'''
-	maskedPhase = np.array(~np.isinf(phase), dtype=int)
-	maskedMs = np.multiply(maskedPhase, ms)
-	maskedMs += 1
-	return maskedMs
-
-def highlightMs(currentMs, initialMs):
-	'''
-	This function 
-		(1) compares a current ms with the initial ms,
-		(2) and masks every element as 1 if the same, retain the same if different
-	'''
-	return highlightedCurrentMs
-
-for i in range(len(npyFolderList) - 1):
-	currentMs = maskMs(phase, np.load(npyFolderList[i]))
-	previousMs = maskMs(phase, np.load(npyFolderList[i-1]))
-	nextMs = maskMs(phase, np.load(npyFolderList[i+1]))
-
-
-
-# cmap = plt.cm.get_cmap("viridis", 5)
-# https://predictablynoisy.com/matplotlib/gallery/color/colormap_reference.html#sphx-glr-gallery-color-colormap-reference-py
-# https://matplotlib.org/stable/tutorials/colors/colormaps.html
-# Ranking: (1) 'coolwarm', (2) 'ocean', (3) 'plasma' or 'inferno' or 'viridis'
-cmap = plt.cm.get_cmap('coolwarm')
-# cmap = plt.cm.get_cmap('viridis')
-# cmap = plt.cm.get_cmap('plasma')
-# cmap = plt.cm.get_cmap('inferno')
-# cmap = plt.cm.get_cmap('ocean')
-# cmap = plt.cm.get_cmap('gnuplot2')
-
-# https://matplotlib.org/cmocean/#thermal
-# import cmocean
-# cmap = cmocean.cm.phase
-
-# for fileName in glob.glob('masked*.vti'): # screenshot for all *.vtr files
-
-reader = pyvista.get_reader(fileName)
-msMesh = reader.read()
-ms = msMesh.get_array('microstructure')
-msMesh.cell_data['microstructure']
-msMesh.set_active_scalars('microstructure', preference='cell')
-grainInfo = np.loadtxt('grainInfo.dat')
-# threshed = msMesh.threshold(value=1.0+1e-3)
-threshed = msMesh.threshold(value=(grainInfo[3], grainInfo[4]))
-# pl = pyvista.Plotter()
-pl = pyvista.Plotter(off_screen=True)
-# pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
-pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
-pl.background_color = "white"
-pl.remove_scalar_bar()
-# pl.camera_position = 'xz'
-# pl.camera.azimuth = -10
-# pl.camera.elevation = +10
-# pl.show(screenshot='%s.png' % fileName[:-4])
-# pl.show()
-if nameTag == '':
-	pl.screenshot(fileName[:-4] + '.png', window_size=[1860*6,968*6])
-else:
-	pl.screenshot(fileName[:-4] + '_' + nameTag + '.png', window_size=[1860*6,968*6])
-# pl.close()
-gc.collect()
-
+j = 0
+for i in range(len(npyFolderList)):
+    currentMs = maskMs(phase, np.load(npyFolderList[i]))
+    previousMs = maskMs(phase, np.load(npyFolderList[i-1]))
+    if np.any(currentMs != previousMs):
+        highlightedCurrentMs = highlightMs(currentMs, initialMs)
+        # nextMs = maskMs(phase, np.load(npyFolderList[i+1]))
+        np.save('highlighted_ms_%d.npy' % j, highlightedCurrentMs)
+        print(f'done {npyFolderList[i]}')
+        j += 1

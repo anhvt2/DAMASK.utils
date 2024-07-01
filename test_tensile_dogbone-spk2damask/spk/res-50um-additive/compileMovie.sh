32a80f341e4ad63237d0bfed0aa13fdbe9d616c9
Anh Tran <anh.vt2@gmail.com>
2024-07-01 18:56:12 -0400

    remove pre-padding zeros

--- test_tensile_dogbone-spk2damask/spk/res-50um-additive/compileMovie.sh
+++ test_tensile_dogbone-spk2damask/spk/res-50um-additive/compileMovie.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 # ffmpeg -framerate 1 -i highlighted_ms_%3d.png -c:v libx264 -r 30 output.mp4
 # ffmpeg -framerate 1/5 -i highlighted_ms_%3d.png -c:v libx264 -r 30 -pix_fmt yuv420p out.mp4
-ffmpeg -i highlighted_ms_%3d.png -c:v libx264  -r 5 -pix_fmt yuv420p out.mp4
+# ffmpeg -i highlighted_ms_%3d.png -c:v libx264  -r 5 -pix_fmt yuv420p out.mp4
+ffmpeg -i highlighted_ms_%d.png -c:v libx264  -r 5 -pix_fmt yuv420p out.mp4
 

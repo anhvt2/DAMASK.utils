6e27be58a1d90732dc676e9812e1924eae44caa3
Anh Tran <anh.vt2@gmail.com>
2024-07-02 22:00:49 -0400

    generalize fileName variable

--- test_ROM_oligocrystal/seedVoid/run.sh
+++ test_ROM_oligocrystal/seedVoid/run.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
-python3 seedVoid.py     --origGeomFileName potts-12_3d.975.geom     --voidPercentage 3     --voidDictionary voidEquiaxed.geom     --phaseFileName phase_dump_12_out.npy
+export spkFileName="potts-12_3d.975"
+python3 seedVoid.py     --origGeomFileName ${spkFileName}.geom     --voidPercentage 3     --voidDictionary voidEquiaxed.geom     --phaseFileName phase_dump_12_out.npy
 
-geom_check voidSeeded_3.000pc_potts-12_3d.975.geom
-python3 plotms3d_maskedDogbone.py --fileName='voidSeeded_3.000pc_potts-12_3d.975.vtr'
+geom_check voidSeeded_3.000pc_${spkFileName}.geom
+python3 plotms3d_maskedDogbone.py --fileName="voidSeeded_3.000pc_${spkFileName}.vtr"
+cat material.config.preamble  | cat - material.config | sponge material.config

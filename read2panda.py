c159b9e240a4d0f29f03f27d2a748da7568a3df3
Anh Tran <anh.vt2@gmail.com>
2024-10-01 15:01:23 -0400

    add read2panda.py: handle/remove duplicate via pandas

--- read2panda.py
+++ read2panda.py
@@ -1,11 +1,24 @@
 
+"""
+    This script 
+        (1) Reads an output from DAMASK and convert it to a panda frame
+        (2) (Optionally) Remove duplicate columns
+        (3) Write to .csv with a relatively close output (w/ changed headers)
+
+"""
+
 import numpy as np
 import pandas as pd
 import time
 import matplotlib as mpl
 import matplotlib.pyplot as plt
+import argparse
+parser = argparse.ArgumentParser()
 
-fileName = 'main_tension.txt'
+parser.add_argument("-f", "--fileName", help='.vtr file', type=str, default='', required=True)
+args = parser.parse_args()
+fileName = args.fileName
+# fileName = 'main_tension_inc01.txt' # debug
 
 fileHandler = open(fileName)
 txt = fileHandler.readlines()
@@ -13,10 +26,23 @@ fileHandler.close()
 
 ### Pre-process
 numHeaderRows = int(txt[0].split('\t')[0])
-headers = txt[numHeaderRows].replace('\n', '').split('\t')
+oldHeader = txt[numHeaderRows].replace('\n', '').split('\t')
 data = np.loadtxt(fileName, skiprows=numHeaderRows+1)
-df = pd.DataFrame(data, columns=headers)
-resolution = 50
-for selStr in ['1_pos', '2_pos', '3_pos']:
-    # df[selStr] -= (resolution/2.)
-    df[selStr] /= (resolution)
+df = pd.DataFrame(data, columns=oldHeader)
+
+# Remove duplicate columns: https://stackoverflow.com/questions/14984119/python-pandas-remove-duplicate-columns
+df = df.loc[:,~df.columns.duplicated()].copy()
+newHeader = list(df)
+
+DamaskCommandHistory = txt[:numHeaderRows]
+DamaskCommandHistory += ['\t'.join(newHeader) + '\n'] # add the last line
+
+f = open(fileName[:-4] + '_header.txt', 'w')
+for i in range(len(DamaskCommandHistory)):
+    f.write(DamaskCommandHistory[i])
+
+f.close()
+
+df.to_csv(fileName[:-4] + '_data.txt', sep='\t', header=False) # do not rewrite header
+# np.savetxt(fileName[:-4] + '_data.txt', df.to_numpy(), fmt='%.16e', delimiter='\t')
+

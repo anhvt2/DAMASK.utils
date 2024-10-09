905741892f41341aa12a448b48c0f2a1e53e21f0
Anh Tran <anh.vt2@gmail.com>
2024-10-09 10:56:20 -0400

    add adaptive nameTag and option for plotting

--- test_ROM_oligocrystal/seedVoid/plotms3d_maskedDogbone.py
+++ test_ROM_oligocrystal/seedVoid/plotms3d_maskedDogbone.py
@@ -38,13 +38,24 @@ msMesh = reader.read()
 ms = msMesh.get_array('microstructure')
 msMesh.cell_data['microstructure']
 msMesh.set_active_scalars('microstructure', preference='cell')
-# threshed = msMesh.threshold(value=1.0+1e-3)
-threshed = msMesh.threshold(value=(grainInfo[1], grainInfo[2]))
+
 # pl = pyvista.Plotter()
 pl = pyvista.Plotter(off_screen=True)
-pl.add_mesh(msMesh.threshold(value=1.0+1e-3), show_edges=True, line_width=1, cmap=cmap, opacity=0.01) # enable dogbone background
-# pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
-pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
+print(f'nameTag = {nameTag}')
+
+if nameTag == 'voids':
+    pl.add_mesh(msMesh.threshold(value=1.0+1e-3), show_edges=True, line_width=1, cmap=cmap, opacity=0.05) # enable dogbone background -- only when plotting voids
+    threshed = msMesh.threshold(value=(grainInfo[1], grainInfo[2])) # plot voids
+    pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
+elif nameTag == 'solids':
+    pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap) # plot solids
+    threshed = msMesh.threshold(value=(grainInfo[3], grainInfo[4])) # plot solids
+    pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
+else:
+    threshed = msMesh.threshold(value=1.0+1e-3) # general settings
+    pl.add_mesh(threshed, show_edges=True, line_width=1, cmap=cmap)
+
+
 pl.background_color = "white"
 pl.remove_scalar_bar()
 # pl.camera_position = 'xz'
@@ -52,13 +63,13 @@ pl.remove_scalar_bar()
 # pl.camera.elevation = +10
 # pl.show(screenshot='%s.png' % fileName[:-4])
 # pl.show()
-pl.add_axes(color='k')
-pl.show_axes() # https://docs.pyvista.org/api/plotting/_autosummary/pyvista.renderer.add_axes
+# pl.add_axes(color='k')
+# pl.show_axes() # https://docs.pyvista.org/api/plotting/_autosummary/pyvista.renderer.add_axes
 
 if nameTag == '':
-	pl.screenshot(fileName[:-4] + '.png', window_size=[1860*6,968*6])
+    pl.screenshot(fileName[:-4] + '.png', window_size=[1860*6,968*6])
 else:
-	pl.screenshot(fileName[:-4] + '_' + nameTag + '.png', window_size=[1860*6,968*6])
+    pl.screenshot(fileName[:-4] + '_' + nameTag + '.png', window_size=[1860*6,968*6])
 # pl.close()
 gc.collect()
 

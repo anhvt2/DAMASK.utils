b55d8f9a009ee3fbd6a48183d68639469d96032b
Anh Tran <anh.vt2@gmail.com>
2024-10-16 10:42:27 -0400

    add lines between cat/sponge

--- test_CBayes_tensileDogbone/spk/makeMaterialsConfig.sh
+++ test_CBayes_tensileDogbone/spk/makeMaterialsConfig.sh
@@ -5,6 +5,8 @@ for i in $(seq 500); do
     cd $folderName
     # cat material.config.preamble  | cat - material.config | sponge material.config
     cp ../material.config.preamble ./material.config
+    echo >> material.config
+    echo >> material.config
     cat material_void.config >> material.config
     cd ..
     echo "done $folderName"

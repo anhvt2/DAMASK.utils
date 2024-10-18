cd9af8ca8a339361615f78f9acfd6868d5473eab
Anh Tran <anh.vt2@gmail.com>
2024-10-17 21:21:54 -0400

    update constructGlobalBasis.py: create snapshot matrix

--- test_ROM_oligocrystal/rom/constructGlobalBasis.py
+++ test_ROM_oligocrystal/rom/constructGlobalBasis.py
@@ -1,5 +1,7 @@
 
 import numpy as np
+import glob
+from natsort import natsorted, ns
 # import pandas as pd
 
 """
@@ -11,16 +13,35 @@ for projection-based ROM.
 TrainIdx = np.loadtxt('TrainIdx.dat')
 FoI = ['Mises(Cauchy)','Mises(ln(V))'] # from export2npy.py
 
+# Count snapshot
 numSnapShot = 0
-
-# data = np.zeros([576000, 1]) # initialize
-# data = np.c_[data, np.zeros(len(data))]
-
 for i in TrainIdx:
     folderName = str(i+1) # from randomizeLoad.py
-    print(f'Processing {i}')
-    fileNameList = natsorted(glob.glob('../damask/%d/postProc/main_tension_inc??.npy' % i)):
+    print(f'Processing {int(i):<d}')
+    fileNameList = natsorted(glob.glob('../damask/%d/postProc/main_tension_inc??.npy' % i))
     numSnapShot += len(fileNameList)
     # for fileName in fileNameList:
     #     d = np.load(fileName) # d = np.load('../damask/1/postProc/main_tension_inc08.npy')
 
+print(f'numSnapShot = {numSnapShot:<d}') # numSnapShot = 11259
+
+# Allocate snapshot matrix
+d_MisesCauchy = np.zeros([576000, numSnapShot])
+d_MisesLnV    = np.zeros([576000, numSnapShot])
+
+# Create snapshot matrices
+j = 0
+for i in TrainIdx:
+    folderName = str(i+1) # from randomizeLoad.py
+    print(f'Processing damask/{int(i):<d}')
+    fileNameList = natsorted(glob.glob('../damask/%d/postProc/main_tension_inc??.npy' % i))
+    for fileName in fileNameList:
+        try:
+            print(f'Processing damask/{int(i):<d}/postProc/{fileName.split("/")[-1]}')
+            d = np.load(fileName) # d = np.load('../damask/1/postProc/main_tension_inc08.npy')
+            # Copy columns of snapshot
+            d_MisesCauchy[:,j] = d[:,0]
+            d_MisesLnV[:,j]    = d[:,1]
+            j += 1
+        except:
+            print(f'Cannot load damask/{int(i):<d}/postProc/{fileName.split("/")[-1]}')

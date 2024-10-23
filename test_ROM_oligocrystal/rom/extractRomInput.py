cac8a1eb5ac24e5cf2939c22cb1c44881c7a6944
Anh Tran <anh.vt2@gmail.com>
2024-10-23 12:10:29 -0400

    consolidate and add comments for building ROM inputs

--- test_ROM_oligocrystal/rom/extractRomInput.py
+++ test_ROM_oligocrystal/rom/extractRomInput.py
@@ -31,51 +31,37 @@ TestIdx  = np.loadtxt('TestIdx.dat', dtype=int)
 FoI = ['Mises(Cauchy)','Mises(ln(V))'] # from export2npy.py
 controlInfo = np.loadtxt('control.log', delimiter=',', skiprows=1)
 
-# Write input for train dataset
-outFileName = 'inputRom_Train.dat'
-f = open(outFileName, 'w')
-f.write('dotVareps, initialT, vareps, sigma\n')
-for i in TrainIdx: # for i in range(1,501):
-    folderName = str(i+1) # taken from randomizeLoad.py
-    logging.info(f'Processing {int(i):<d}')
-    fileName = '../damask/%d/postProc/stress_strain.log' % i
-    dotVareps = controlInfo[i,1]
-    initialT  = controlInfo[i,3]
-    if os.path.exists(fileName):
-        fileHandler = open(fileName)
-        txt = fileHandler.readlines()
-        fileHandler.close()
-        numHeaderRows = int(txt[0].split('\t')[0])
-        oldHeader = txt[numHeaderRows].replace('\n', '').split('\t')
-        data = np.loadtxt(fileName, skiprows=numHeaderRows+1)
-        strain = data[:,1]
-        stress = data[:,2]
-        for j in range(len(strain)):
-            f.write('%.8e, %8.e, %8.e, %.1f\n'% (dotVareps, initialT, strain[j], stress[j]))
+for MlType in ['Train', 'Test']:
+    # Write input for train/test dataset
+    outFileName = 'inputRom_%s.dat' % MlType
+    f = open(outFileName, 'w')
+    f.write('dotVareps, initialT, vareps, sigma, DamaskIndex, PostProcIndex\n')
+    # Switch between data type
+    if MlType == 'Train':
+        idx = TrainIdx
+    elif MlType == 'Test':
+        idx = TestIdx
+    for i in idx: # for i in range(1,501):
+        folderName = str(i+1) # taken from randomizeLoad.py
+        logging.info(f'Processing {int(i):<d}')
+        fileName = '../damask/%d/postProc/stress_strain.log' % i
+        dotVareps = controlInfo[i,1]
+        initialT  = controlInfo[i,3]
+        # Check if 'stress_strain.log' exists
+        if os.path.exists(fileName):
+            fileHandler = open(fileName)
+            txt = fileHandler.readlines()
+            fileHandler.close()
+            numHeaderRows = int(txt[0].split('\t')[0])
+            oldHeader = txt[numHeaderRows].replace('\n', '').split('\t')
+            data = np.loadtxt(fileName, skiprows=numHeaderRows+1)
+            # Extract relevant data
+            inc = data[:,0]
+            strain = data[:,1]
+            stress = data[:,2]
+            # Write to output file
+            for j in range(1,len(strain)):
+                f.write('%.8e, %.8e, %.8e, %.1f, %d, %d\n'% (dotVareps, initialT, strain[j], stress[j], i, inc[j]))
 
-f.close()
+    f.close()
 
-# Write input for test dataset
-outFileName = 'inputRom_Test.dat'
-f = open(outFileName, 'w')
-f.write('dotVareps, initialT, vareps, sigma\n')
-for i in TestIdx:
-# for i in range(1,501):
-    folderName = str(i+1) # from randomizeLoad.py
-    logging.info(f'Processing {int(i):<d}')
-    fileName = '../damask/%d/postProc/stress_strain.log' % i
-    dotVareps = controlInfo[i,1]
-    initialT  = controlInfo[i,3]
-    if os.path.exists(fileName):
-        fileHandler = open(fileName)
-        txt = fileHandler.readlines()
-        fileHandler.close()
-        numHeaderRows = int(txt[0].split('\t')[0])
-        oldHeader = txt[numHeaderRows].replace('\n', '').split('\t')
-        data = np.loadtxt(fileName, skiprows=numHeaderRows+1)
-        strain = data[:,1]
-        stress = data[:,2]
-        for j in range(len(strain)):
-            f.write('%.8e, %8.e, %8.e, %.1f\n'% (dotVareps, initialT, strain[j], stress[j]))
-
-f.close()

8c384b768d868409a39624106271c1d8284f0905
Anh Tran <anh.vt2@gmail.com>
2024-10-24 16:26:03 -0400

    update computeBasis.py based on vizEigenDecay.py

--- test_ROM_oligocrystal/rom/computeBasis.py
+++ test_ROM_oligocrystal/rom/computeBasis.py
@@ -9,6 +9,9 @@ import sklearn.utils.extmath as skmath
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 
+mpl.rcParams['xtick.labelsize'] = 18
+mpl.rcParams['ytick.labelsize'] = 24
+
 fois = ['MisesCauchy', 'MisesLnV'] # fields of interest
 # fois = ['MisesCauchy'] # fields of interest
 # fois = ['MisesLnV'] # fields of interest
@@ -43,32 +46,36 @@ for foi in fois:
     # Verify that: d = np.dot(u, np.dot(np.diag(s), vT))
     fig, ax1 = plt.subplots(num=None, figsize=(16, 9), dpi=300, facecolor='w', edgecolor='k')
     ax1.plot(s)
-    ax1.plot(s, c='b', marker='o', markersize=1)
-    ax1.set_xscale('log')
+    ax1.plot(s, c='b', marker='o', linestyle='None', markersize=1, label=r'eigenvalue')
+    # ax1.set_xscale('log')
     ax1.set_yscale('log')
-    ax1.set_xlabel('index', fontsize=12)
-    ax1.set_ylabel(r'$\sigma_i$', fontsize=12)
-    ax1.set_title('eigenspectrum', fontsize=12)
+    ax1.set_xlabel('POD components', fontsize=24)
+    ax1.set_ylabel(r'Eigenvalue', fontsize=24)
+    ax1.set_title(r'Eigenspectrum: %s' % label, fontsize=24)
+    ax1.set_xlim(left=0,right=int(len(s)/1.2))
+    ax1.grid(None)
 
     ax2 = ax1.twinx()
-    ax2.plot(np.cumsum(s) / np.sum(s), c='tab:green', marker='s', markersize=1)
-    ax2.set_ylabel(r'relative "energy" ($\ell_2$)', fontsize=12)
+    ax2.plot(np.cumsum(s) / np.sum(s), c='tab:green', linestyle='None', marker='s', markersize=1, label=r'relative energy')
     ax2.set_ylim(top=1.0)
+    ax2.xaxis.set_major_locator(mpl.ticker.MultipleLocator(500))
+    ax2.xaxis.set_minor_locator(mpl.ticker.MultipleLocator(250))
+    # ax2.yaxis.set_major_locator(mpl.ticker.MultipleLocator(0.05))
+    ax2.set_ylabel(r'relative "energy" ($\ell_2$)', fontsize=24)
+    ax2.grid(visible=True, which='both', axis='x', linestyle='-', linewidth='2')
     # plt.show()
+    lines1, labels1 = ax1.get_legend_handles_labels()
+    lines2, labels2 = ax2.get_legend_handles_labels()
+    ax2.legend(lines1 + lines2, labels1 + labels2, fontsize=24, markerscale=10, loc='best')
     plt.savefig('eigendecay_%s.png' % foi, dpi=300, facecolor='w', edgecolor='w', 
         orientation='landscape', format=None, transparent=False, 
         bbox_inches='tight', pad_inches=0.1, metadata=None)
+    # Diagnostics
+    q = np.cumsum(s) / np.sum(s) # quantile
+    # Print quantile with POD components
+    for qPrint in [0.90, 0.95, 0.99, 0.995]:
+        print(f'{foi:10}: {qPrint*100:<.1f}% quantile = {np.where(q>qPrint)[0][0]:<d} POD components')
+    print('\n')
+
 
 print(f'Total time for POD basis: {time.time() - t_start:<.2f} seconds.')
-
-# Loading time: 221.89 seconds.
-# Non-zero elements = 5524 elements.
-# Centering time: 34.98 seconds.
-# SVD time: 633.08 seconds.
-# Save time: 215.23 seconds.
-# Loading time: 1334.92 seconds.
-# Non-zero elements = 5524 elements.
-# Centering time: 24.71 seconds.
-# SVD time: 594.55 seconds.
-# Save time: 220.47 seconds.
-# Total time for POD basis: 2177.61 seconds.
--- test_ROM_oligocrystal/rom/computeCoefs.py
+++ test_ROM_oligocrystal/rom/computeCoefs.py
@@ -4,9 +4,10 @@ import os, glob
 import time
 import logging
 
+t_start = time.time()
 level    = logging.INFO
 format   = '  %(message)s'
-logFileName = 'extractRomInput.py.log'
+logFileName = 'computeCoefs.py.log'
 os.system('rm -fv %s' % logFileName)
 handlers = [logging.FileHandler(logFileName), logging.StreamHandler()]
 logging.basicConfig(level = level, format = format, handlers = handlers)
@@ -14,6 +15,21 @@ logging.basicConfig(level = level, format = format, handlers = handlers)
 basis_MisesCauchy = np.load('podBasis_MisesCauchy.npy')
 basis_MisesLnV = np.load('podBasis_MisesLnV.npy')
 
-for 
-    if d.shape == (576000,2): # safeguard -- check shape
+romPath = os.getcwd() + '/'
+damaskPath  = romPath + '../damask/'
+os.chdir(damaskPath)
+for i in range(1,1001):
+    print(f'Computing POD coefs in damask/{int(i):<d}/')
+    os.chdir(damaskPath + '%s/postProc/')
+    for fileName in natsorted(glob.glob('main_tension_inc??.npy' % i)):
+        tmpData = np.load(fileName)
+        if tmpData.shape == (576000,2): # safeguard -- check shape
+            podCoefs_MisesCauchy = np.dot(basis_MisesCauchy, tmpData[:,0])
+            podCoefs_MisesLnV    = np.dot(basis_MisesLnV,    tmpData[:,1])
+            podCoefs = np.hstack((podCoefs_MisesCauchy, podCoefs_MisesLnV))
+            print(f'Finish calculating POD coefs in damask/{int(i):<d}/postProc/{fileName}')
+            np.save('podCoefs_' + fileName.split('.')[:-4], podCoefs)
 
+    os.chdir(damaskPath)
+
+logging.info(f'computeCoefs.py: Elapsed time = {time.time() - t_start} seconds.')

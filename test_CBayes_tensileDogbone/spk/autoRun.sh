1a1e349540b17ceb9ff9099c7618597cd6067a4a
Anh Tran <anh.vt2@gmail.com>
2024-12-11 15:00:45 -0500

    convert indent to spaces

--- test_CBayes_tensileDogbone/spk/autoRun.sh
+++ test_CBayes_tensileDogbone/spk/autoRun.sh
@@ -1,20 +1,20 @@
 #!/bin/bash
 for folderName in $(ls -1dv test-run-*-*/); do
-	cd ${folderName}
+    cd ${folderName}
 
-	ln -sf padded*.geom main.geom # change geom fileName
+    ln -sf padded*.geom main.geom # change geom fileName
     # cp -v ../../sbatch.damask.srn .
-	rm sbatch.damask.srn
-	rm sbatch.postProcDamaskLocalQoIs.srn
-	# cp -v ../../sbatch.postProcDamaskLocalQoIs.srn . # Local QoIs
-	cp -v ../../sbatch.postProcDamaskGlobalQoIs.srn . # Global QoIs
+    rm sbatch.damask.srn
+    rm sbatch.postProcDamaskLocalQoIs.srn
+    # cp -v ../../sbatch.postProcDamaskLocalQoIs.srn . # Local QoIs
+    cp -v ../../sbatch.postProcDamaskGlobalQoIs.srn . # Global QoIs
     ln -sf ../../tension.load .
     ln -sf ../../numerics.config .
     ln -sf ../../numProcessors.dat .
-	ln -sf ../../gaugeFilter.txt .
-	ssubmit
+    ln -sf ../../gaugeFilter.txt .
+    ssubmit
 
-	cd ..
-	echo "done ${folderName}"
+    cd ..
+    echo "done ${folderName}"
 done
 

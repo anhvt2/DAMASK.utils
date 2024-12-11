00728136c837f17a5366b6eedccac0bb44f47e23
Anh Tran <anh.vt2@gmail.com>
2024-12-11 14:51:13 -0500

    update test_CBayes_tensileDogbone/spk/autoRun.sh structure

--- test_CBayes_tensileDogbone/spk/autoRun.sh
+++ test_CBayes_tensileDogbone/spk/autoRun.sh
@@ -1,29 +1,20 @@
 #!/bin/bash
-# for spkFolder in $(ls -1dv */); do
-for i in $(seq 100); do
-	spkFolder="res-50um-run-${i}"
-	cd ${spkFolder}
+for folderName in $(ls -1dv test-run-*-*/); do
+	cd ${folderName}
 
-	for geomFolder in $(ls -1dv */); do
-		cd ${geomFolder}
-
-		ln -sf padded*.geom main.geom # change geom fileName
-        # cp -v ../../sbatch.damask.srn .
-		rm sbatch.damask.srn
-		rm sbatch.postProcDamaskLocalQoIs.srn
-		# cp -v ../../sbatch.postProcDamaskLocalQoIs.srn . # Local QoIs
-		cp -v ../../sbatch.postProcDamaskGlobalQoIs.srn . # Global QoIs
-        ln -sf ../../tension.load .
-        ln -sf ../../numerics.config .
-        ln -sf ../../numProcessors.dat .
-		ln -sf ../../gaugeFilter.txt .
-		ssubmit
-
-		cd ..
-		echo "done ${spkFolder}/${geomFolder}"
-	done
+	ln -sf padded*.geom main.geom # change geom fileName
+    # cp -v ../../sbatch.damask.srn .
+	rm sbatch.damask.srn
+	rm sbatch.postProcDamaskLocalQoIs.srn
+	# cp -v ../../sbatch.postProcDamaskLocalQoIs.srn . # Local QoIs
+	cp -v ../../sbatch.postProcDamaskGlobalQoIs.srn . # Global QoIs
+    ln -sf ../../tension.load .
+    ln -sf ../../numerics.config .
+    ln -sf ../../numProcessors.dat .
+	ln -sf ../../gaugeFilter.txt .
+	ssubmit
 
 	cd ..
-	echo "done ${spkFolder}"
+	echo "done ${folderName}"
 done
 

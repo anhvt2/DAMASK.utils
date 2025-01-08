0e5d7ecedbdcf19f2003fbe3f7fbb442aebf3cc8
Anh Tran <anh.vt2@gmail.com>
2025-01-08 18:10:12 -0500

    add targetPoro into local porosity.txt

--- test_CBayes_tensileDogbone/spk/computeGaugePorosity.py
+++ test_CBayes_tensileDogbone/spk/computeGaugePorosity.py
@@ -11,9 +11,12 @@ $ cat gaugeFilter.txt
 NOTE:
     Due to some bugs, the air voxels are indexed as 1 and 2, instead of just 1. In order to scope with this bug, increase the threshold to 3. Debug ongoing.
 """
-
+porosity = np.loadtxt('../seedVoid/porosity.txt')
 grainInfo = np.loadtxt('grainInfo.dat')
 geom = np.load(glob.glob('htt*.npy')[0])
+
+runIdx = int(os.getcwd().split('run-')[-1].split('-')[0]) - 1 # reset start index = 0
+targetPoro = porosity[runIdx]
 
 # Global: Count how many void voxels in the WHOLE domain
 numSolidVoxels = np.where((geom >= grainInfo[3]))[0].shape[0]
@@ -24,14 +27,17 @@ globalPoro = numVoidVoxels / numSolidVoxels
 gaugeGeom = geom[51:69+1, 2:21+1, 70:130+1]
 numSolidVoxels = np.where((gaugeGeom >= grainInfo[3]))[0].shape[0]
 numVoidVoxels = np.where((gaugeGeom >= 3) & (gaugeGeom <= grainInfo[2]))[0].shape[0]
-localPoro = numVoidVoxels / numSolidVoxels
+localPoro = numVoidVoxels / numSolidVoxels1
 
 print(f'In {os.getcwd().split('/')[-1]}:')
 print(f'(whole specimen) Global porosity = {globalPoro:<.4f}')
 print(f'(gauge section) Local porosity = {localPoro:<.4f}')
+print(f'(whole specimen) Target porosity = {targetPoro:<.4f}')
+print('\n')
 
 f = open('porosity.txt', 'w')
 f.write(f'{globalPoro:<.4f}\n')
-f.write(f'{localPoro:<.4f}\n\n')
+f.write(f'{localPoro:<.4f}\n')
+f.write(f'{targetPoro:<.4f}\n')
 f.close()
 

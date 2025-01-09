df955e1056f6dce63f5fb00606f483e713e1d75d
Anh Tran <anh.vt2@gmail.com>
2025-01-09 06:52:45 -0700

    add test_CBayes_tensileDogbone/analysis/ for stochastic inverse analysis

---
+++ test_CBayes_tensileDogbone/analysis/copyFiles.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+mkdir -p train
+mkdir -p test
+cd train/
+for i in $(seq 500); do
+    mkdir -p ${i}
+    cd ${i}
+    cp -fv ../../../spk/res-50um-additive-run-${i}/porosity.txt .
+    cp -fv ../../../spk/res-50um-additive-run-${i}/postProc/main_tension.txt .
+    cp -fv ../../../spk/res-50um-additive-run-${i}/postProc/stress_strain.log .
+    cd ..
+done
+
+cd ..
+cd test/
+
+for testIdx in $(cat testFolders.txt); do
+    testFolder="test-run-${testIdx}"
+    cd ${testFolder}
+    cp -fv ../../../spk/${testFolder}/porosity.txt .
+    cp -fv ../../../spk/${testFolder}/postProc/main_tension.txt .
+    cp -fv ../../../spk/${testFolder}/postProc/stress_strain.log .
+    cd ..
+done
+
+cd ..
+

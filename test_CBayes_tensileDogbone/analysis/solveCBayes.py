77fd94ac77ef08bb9c48b40ef193dd46d06ed4ac
Anh Tran <anh.vt2@gmail.com>
2025-01-21 23:33:19 -0500

    add solveCBayes.py

---
+++ test_CBayes_tensileDogbone/analysis/solveCBayes.py
@@ -0,0 +1,13 @@
+
+import numpy as np
+import glob, os, sys
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+import sklearn
+from scipy.stats import norm # The standard Normal distribution
+from scipy.stats import gaussian_kde as GKDE # A standard kernel density estimator
+
+mpl.rcParams['xtick.labelsize'] = 24
+mpl.rcParams['ytick.labelsize'] = 24 
+
+

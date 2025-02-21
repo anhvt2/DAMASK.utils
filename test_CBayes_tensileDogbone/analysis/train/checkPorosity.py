2a481716c038a3e0b3ce7f8b1a73feff0780e36c
Anh Tran <anh.vt2@gmail.com>
2025-02-21 12:57:48 -0500

    polish parity plot of porosity

--- test_CBayes_tensileDogbone/analysis/train/checkPorosity.py
+++ test_CBayes_tensileDogbone/analysis/train/checkPorosity.py
@@ -53,22 +53,34 @@ plt.savefig('porosities.png', dpi=300, facecolor='w', edgecolor='w', orientation
 mpl.rcParams['xtick.labelsize'] = 12
 mpl.rcParams['ytick.labelsize'] = 12
 plt.figure(num=None, figsize=(168, 100), dpi=300, facecolor='w', edgecolor='k')
-# sns.jointplot(data=df, x="global", y="local", kind='kde',
-#     marginal_kws={
-#     'fill': True,    # Fill the area under the KDE curve
-#     'color': 'purple',  # Set the color of the KDE curve
-#     'alpha': 0.6,    # Set the transparency of the filled area
-#     'linewidth': 2   # Set the line width of the KDE curve)
-#     })
+g = sns.jointplot(data=df, x="global", y="local", kind='reg', 
+    scatter_kws={'s': 3},
+    joint_kws={'color': 'tab:gray'},
+    marginal_kws={
+    'bins': 23,
+    'fill': True,    # Fill the area under the KDE curve
+    'color': 'tab:gray',  # Set the color of the KDE curve
+    'alpha': 0.2,    # Set the transparency of the filled area
+    'linewidth': 2   # Set the line width of the KDE curve)
+    },
+    )
+
 # g = sns.jointplot(data=df, x="global", y="local", kind='scatter', color='blue')
-g = sns.jointplot(data=df, x="global", y="local", kind='scatter', color='tab:blue', 
-    alpha=0.8,
-    marginal_kws=dict(bins=50, fill=True))
-    # marginal_kws={'fill': True, 'color': 'blue', 'alpha': 0.6, 'linewidth': 2})
-g.plot_marginals(sns.kdeplot, fill=True, color='tab:blue', alpha=0.8, linewidth=1, cut=0)
+# g = sns.jointplot(data=df, x="global", y="local", kind='scatter', color='tab:blue', 
+#     alpha=0.8,
+#     marginal_kws=dict(bins=50, fill=True)
+#     )
+#     # marginal_kws={'fill': True, 'color': 'blue', 'alpha': 0.6, 'linewidth': 2})
+# g.plot_marginals(sns.kdeplot, fill=True, color='tab:blue', alpha=0.8, linewidth=1, cut=0)
+
+# g = sns.jointplot(
+#     data=df, x="global", y="local", kind='resid', color='tab:blue', alpha=0.8
+# )
+
+# g.plot_marginals(sns.kdeplot, fill=True, color='tab:blue', alpha=0.8, linewidth=1, cut=0)
+
+
 # sns.kdeplot(data=df, x="global", y="local", ax=g.ax_joint, color='blue', fill=True, alpha=0.8, linewidth=1)
-
-
 
 # sns.jointplot(data=df, x="global", y="local", kind='scatter', 
 #     marginal_kind='kde',
@@ -80,4 +92,7 @@ g.plot_marginals(sns.kdeplot, fill=True, color='tab:blue', alpha=0.8, linewidth=
 #     })
 plt.xlabel(r'global $\phi$', fontsize=12)
 plt.ylabel(r'local (gauge) $\phi$', fontsize=12)
+plt.xlim(left=0,right=0.05)
+plt.ylim(bottom=0,top=0.05)
 plt.savefig('JointplotPorosities.png', dpi=300, facecolor='w', edgecolor='w', orientation='portrait', format=None, transparent=False, bbox_inches='tight', pad_inches=0.1, metadata=None)
+

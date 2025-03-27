41cfe4d6a7c62743c7d26f882e08569e61458039
Anh Tran <anh.vt2@gmail.com>
2025-03-26 21:55:10 -0400

    initialize test_FNO/ for Fourier neural operator

---
+++ test_FNO/3d/generateMsDream3d.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+
+## declare parameters
+# two params for controlling log-normal distribution
+mu=$(cat mu.dat)
+sigma=$(cat sigma.dat)
+msId=$(cat msId.dat)
+
+dimCell=64
+
+
+## declare paths
+
+# execPath="/ascldap/users/anhtran/data/DREAM.3D/DREAM3D-6.5.138-Linux-x86_64/bin"
+execPath="/home/anhvt89/Documents/DREAM.3D/DREAM3D-6.5.141-Linux-x86_64/bin"
+outputPath=$(pwd)
+
+inputFile="damask.json"
+currentPath="${inputPath}"
+
+
+echo "Running $inputFile"
+echo "Output path: $outputPath"
+echo
+echo "Parameters settings:"
+echo "mu = $mu"
+echo "sigma = $sigma"
+echo "dimCell = $dimCell"
+echo
+
+
+${execPath}/PipelineRunner -p $(pwd)/${inputFile}
+
+# cat material.config.preamble  | cat - material.config | sponge material.config
+
+
+# echo "Microstructure files are generated at:"
+# echo "$outputPath"
+# echo
+# echo
+
+# geom_check *.geom
+# bash getDream3dInfo.sh
---
+++ test_FNO/README.md
@@ -0,0 +1,12 @@
+
+This respository aims to test the applicability of FNO for CPFEM with different microstructures and mesh resolution.
+
+Loading condition is to be fixed to uniaxial loading condition.
+
+2D:
+- 32x32
+- 16x16
+
+3D:
+- 32x32x32
+- 16x16x16

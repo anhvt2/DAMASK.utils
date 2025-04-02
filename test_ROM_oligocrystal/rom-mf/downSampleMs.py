b6c7a0062b0fb6b039cf7cff841d91906f7cf1ad
Anh Tran <anh.vt2@gmail.com>
2025-04-02 11:55:53 -0400

    remove air buffer before down-sampling

--- test_ROM_oligocrystal/rom-mf/downSampleMs.py
+++ test_ROM_oligocrystal/rom-mf/downSampleMs.py
@@ -5,6 +5,15 @@ import subprocess
 
 # High-fidelity: 50um
 mhf = np.load('mhf-50um.npy')
+mhf.shape # debug (120, 24, 200) # need to remove air buffer layer
+mhf = mhf[:,2:-2, :]
+outFileName = 'mhf-50um'
+# Save high-fidelity ms
+np.save(outFileName + '.npy', mhf)
+subprocess.run(["python3", "../../npy2geom.py", "--npy", f"{outFileName}.npy"], check=True)
+subprocess.run(["python3", "../../npy2png.py", "--threshold", "1", "--npy", f"{outFileName}.npy"], check=True)
+subprocess.run(["geom_check", f"{outFileName}.geom"], check=True)
+
 zoomFactors  = np.array([0.5, 0.25]) # 0.125 with 400um too coarse
 resolutions  = 50 / zoomFactors
 
@@ -12,9 +21,6 @@ for zoomFactor, resolution in zip(zoomFactors, resolutions):
     mlf = scipy.ndimage.zoom(mhf, zoom=(zoomFactor, zoomFactor, zoomFactor), mode='constant', order=0)
     outFileName = f'mlf-{resolution.astype(int)}um'
     np.save(outFileName + '.npy', mlf)
-    # os.system(f'python3 ../../npy2geom.py --npy {outFileName}.npy')
-    # os.system(f'python3 ../../npy2png.py --threshold 1 --npy {outFileName}.npy')
-    # os.system(f'geom_check {outFileName}.geom')
     subprocess.run(["python3", "../../npy2geom.py", "--npy", f"{outFileName}.npy"], check=True)
     subprocess.run(["python3", "../../npy2png.py", "--threshold", "1", "--npy", f"{outFileName}.npy"], check=True)
     subprocess.run(["geom_check", f"{outFileName}.geom"], check=True)

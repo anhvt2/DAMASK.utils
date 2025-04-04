b513736a15579ae5901eaff855fc36dd145d3b12
Anh Tran <anh.vt2@gmail.com>
2025-04-04 16:19:08 -0400

    make test_ROM_MF/seedVoid.py independent

--- test_ROM_MF/seedVoid/run.sh
+++ test_ROM_MF/seedVoid/run.sh
@@ -4,24 +4,24 @@
 export spkFileName="dump.additive_dogbone.2807"
 python3 seedVoid.py \
     --origGeomFileName ${spkFileName}.geom \
-    --voidPercentage 5.000 \
+    --voidPercentage 3.000 \
     --voidDictionary voidEquiaxed.geom \
     --phaseFileName phase_dump_12_out.npy
 
 python3 padAirPolycrystals.py \
     --numAirVoxels=4 \
-    --origGeomFileName="voidSeeded_5.000pc_${spkFileName}.geom"
+    --origGeomFileName="voidSeeded_3.000pc_${spkFileName}.geom"
 
-geom_check voidSeeded_5.000pc_${spkFileName}.geom
+geom_check voidSeeded_3.000pc_${spkFileName}.geom
 
-geom_check padded_voidSeeded_5.000pc_${spkFileName}.geom
+geom_check padded_voidSeeded_3.000pc_${spkFileName}.geom
 
-python3 geom2npy.py --geom="padded_voidSeeded_5.000pc_${spkFileName}.geom"
+python3 geom2npy.py --geom="padded_voidSeeded_3.000pc_${spkFileName}.geom"
 
-# python3 findGaugeLocations.py --geom="padded_voidSeeded_5.000pc_${spkFileName}.npy" --resolution 50
+# python3 findGaugeLocations.py --geom="padded_voidSeeded_3.000pc_${spkFileName}.npy" --resolution 50
 
-python3 plotms3d_maskedDogbone.py --fileName="padded_voidSeeded_5.000pc_${spkFileName}.vtr"
+python3 plotms3d_maskedDogbone.py --fileName="padded_voidSeeded_3.000pc_${spkFileName}.vtr"
 
 cat material.config.preamble  | cat - material.config | sponge material.config
 
-cp padded_voidSeeded_5.000pc_${spkFileName}.geom main.geom
+cp padded_voidSeeded_3.000pc_${spkFileName}.geom main.geom

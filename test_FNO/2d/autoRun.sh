e9c4854a7b131f38ab14428eaad2a670531ca353
Anh Tran <anh.vt2@gmail.com>
2025-04-12 09:34:12 -0600

    debug: autoRun.sh for submitting jobs in parallel

--- test_FNO/2d/autoRun.sh
+++ test_FNO/2d/autoRun.sh
@@ -12,28 +12,14 @@ for ((i=1; i<=${numSimulations}; i++)); do
 
     # Move file to local folder
     mkdir -p ${i}
-    mv dream3d.material.config material.config simple2d.geom $i
+    mv material.config simple2d.geom $i
 
-    # Run simulations
+    # # Run simulations
     cd ${i}
-    cp ../tension.load .
-    geom_check simple2d.geom
-    python3 ../../../geom2png.py --geom simple2d.geom
-    $DAMASK_spectral --load tension.load --geom simple2d.geom
-    postResults *.spectralOut --cr f,p
-
-    cd postProc/
-    addStrainTensors simple2d_tension.txt --left --logarithmic
-    addCauchy simple2d_tension.txt
-    addMises simple2d_tension.txt --strain 'ln(V)' --stress Cauchy
-    filterTable < simple2d_tension.txt --white inc,'Mises(ln(V))','Mises(Cauchy)' > stress_strain.log
-    cd ..
-    postResults \
-        --cr fp,f,p,grainrotation,texture \
-        --split \
-        --separation x,y,z \
-        --increments \
-        --range 1 30 1 simple2d_tension.spectralOut 2>&1 > log.postResults
+	cp ../tension.load .
+	cp ../numProcessors.dat .
+	cp ../sbatch.damask.srn .
+	# ssubmit
     cd ..
 
     echo "End simulation $i"

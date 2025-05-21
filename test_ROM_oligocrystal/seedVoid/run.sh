59b859198b44c539bbda6a798f120a3d55316f3a
Anh Tran <anh.vt2@gmail.com>
2025-05-21 10:07:52 -0400

    fix utils-pep8/geom2npy.py

--- test_ROM_oligocrystal/seedVoid/run.sh
+++ test_ROM_oligocrystal/seedVoid/run.sh
@@ -28,7 +28,7 @@ python3 geom2npy.py --geom="padded_voidSeeded_${voidPc}pc_${spkFileName}.geom"
 
 python3 findGaugeLocations.py --geom="padded_voidSeeded_${voidPc}pc_${spkFileName}.npy" --resolution 50
 
-python3 plotms3d_maskedDogbone.py --fileName="padded_voidSeeded_${voidPc}pc_${spkFileName}.vtr"
+python3 plotms3d_maskedDogbone.py --filename="padded_voidSeeded_${voidPc}pc_${spkFileName}.vtr"
 
 cat material.config.preamble  | cat - material.config | sponge material.config
 
--- utils-pep8/geom2npy.py
+++ utils-pep8/geom2npy.py
@@ -1,25 +1,62 @@
 #!/usr/bin/env python3
 
+import numpy as np
+import glob
+import os
+from natsort import natsorted, ns  # natural-sort
+import pyvista
 import argparse
 
-  # natural-sort
-import numpy as np
+'''
+Example
+-------
 
-PARSER = argparse.ArgumentParser()
-PARSER.add_argument("-g", "--geom", type=str, required=True)
-ARGS = PARSER.parse_args()
+python3 geom2npy.py --geom spk_dump_12_out.geom
 
-FILE_NAME = ARGS.geom
+Parameters
+----------
+--geom: geometry file
+
+Return
+------
+microstructure in 3d numpy array: spk_dump_12_out.npy
+vti file: spk_dump_12_out.vti
+
+'''
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-g", "--geom", type=str, required=True)
+args = parser.parse_args()
+
+fileName = args.geom
 
 
-def _save_array2_vti(file_name, array):
+def save_array2vti(file_name, array):
     import vtk
-    from vtk.util.numpy_support import numpy_to_vtk
+    from vtk.util.numpy_support import vtk_to_numpy, numpy_to_vtk
+    import numpy as np
+    """
+	Credit to Leidong Xu (UConn) with some generalizations and correction
+	(1) .ravel() -> .T.flatten()
+	(2) SetDimension(array.shape)
 
-    vtk_data_array = numpy_to_vtk(array.T.flatten(), deep=True, array_type=vtk.VTK_INT)
+	Save a 3D numpy array to a VTI file.
+
+	Args:
+	- file_name (str): Path where the VTI file should be saved.
+	- array (np.ndarray): 3D numpy array to be saved.
+
+	Returns:
+	- None
+	"""
+    # Convert the numpy array to a VTK array
+    vtk_data_array = numpy_to_vtk(
+        array.T.flatten(), deep=True, array_type=vtk.VTK_INT)
+    # Create an image data object and set its dimensions and scalars
     image_data = vtk.vtkImageData()
     image_data.SetDimensions(array.shape)
     image_data.GetPointData().SetScalars(vtk_data_array)
+    # Initialize the VTI writer, set the filename and input data
     writer = vtk.vtkXMLImageDataWriter()
     writer.SetFileName(file_name)
     writer.SetInputData(image_data)
@@ -27,7 +64,7 @@ def _save_array2_vti(file_name, array):
     return None
 
 
-def _delete(lst, to_delete):
+def delete(lst, to_delete):
     '''
     Recursively delete an element with content described by  'to_delete' variable
     https://stackoverflow.com/questions/53265275/deleting-a-value-from-a-list-using-recursion/
@@ -43,30 +80,31 @@ def _delete(lst, to_delete):
 
 
 # deprecate fileName.split('.')[0] to avoid '.' in outFileName
-OUT_FILE_NAME = FILE_NAME[:-5]
-with open(FILE_NAME) as fileHandler:
-    txt = fileHandler.readlines()
-
-
-NUM_SKIPPING_LINES = int(txt[0].split(' ')[0]) + 1
+outFileName = fileName[:-5]
+fileHandler = open(fileName)
+txt = fileHandler.readlines()
+fileHandler.close()
+numSkippingLines = int(txt[0].split(' ')[0])+1
 # Search for 'size' within header:
-for j in range(NUM_SKIPPING_LINES):
+for j in range(numSkippingLines):
     if 'grid' in txt[j]:
-        cleanString = _delete(txt[j].replace('\n', '').split(' '), '')
+        cleanString = delete(txt[j].replace('\n', '').split(' '), '')
         Nx = int(cleanString[2])
         Ny = int(cleanString[4])
         Nz = int(cleanString[6])
 
-GEOM_BLOCK = txt[NUM_SKIPPING_LINES:]
-GEOM = sum(GEOM_BLOCK)
+geomBlock = txt[numSkippingLines:]
+geom = ''
+for i in range(len(geomBlock)):
+    geom += geomBlock[i]
 
-GEOM = GEOM.split(' ')
-GEOM = list(filter(('').__ne__, GEOM))
+geom = geom.split(' ')
+geom = list(filter(('').__ne__, geom))
 
 # Convert from 1 line format to 3d format
-GEOM = np.array(GEOM, dtype=int).reshape(Nz, Ny, Nx).T
-  # to reverse: geom = geom.T.flatten()
+geom = np.array(geom, dtype=int).reshape(
+    Nz, Ny, Nx).T  # to reverse: geom = geom.T.flatten()
 
 # Save output
-np.save(OUT_FILE_NAME + '.npy', GEOM)
-_save_array2_vti(OUT_FILE_NAME + '.vti', GEOM)
+np.save(outFileName + '.npy', geom)
+save_array2vti(outFileName + '.vti', geom)

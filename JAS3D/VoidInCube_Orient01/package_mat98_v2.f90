29a0805f1d0d598967fa1a92338b08dd6d2f53df
Anh Tran <anh.vt2@gmail.com>
2025-07-28 19:08:36 -0400

    remove JAS3D

--- JAS3D/VoidInCube_Orient01/package_mat98_v2.f90
+++
@@ -1,8 +0,0 @@
-INCLUDE 'mat98_init_v2.f90'
-INCLUDE 'mat98_main_v2.f90'
-INCLUDE 'mat98_matint_v2.f90'
-INCLUDE 'mat98_subroutines_v2.f90'
-INCLUDE 'mat98_svinit_avg_v3i.f90'
-INCLUDE 'mat98_updstr_v2.f90'
-
-
